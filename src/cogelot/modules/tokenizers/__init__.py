ab4cc1893b8a5754c5b06759bc4394b467cbdcf7
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-06-02 16:41:31 +0100

    feat(modules/tokenizers): add text tokenizer module from refactored implemenation

---
+++ src/cogelot/modules/tokenizers/__init__.py
@@ -0,0 +1,4 @@
+from cogelot.modules.tokenizers.text import (
+    AssetPlaceholderToken,
+    add_placeholder_tokens_to_tokenizer,
+)
---
+++ src/cogelot/modules/tokenizers/text.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel
+from tokenizers import AddedToken, Tokenizer
+
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+LEFT_SYMBOL = "{"
+RIGHT_SYMBOL = "}"
+
+
+class AssetPlaceholderToken(BaseModel):
+    """Placeholder token.
+
+    If there is more than one repeat needed (e.g., prefixed with "_1" etc), then set the `max_idx`
+    to the maximum index number that will exist.
+    """
+
+    asset_name: str
+    max_idx: int | None = None
+    left_symbol: str = LEFT_SYMBOL
+    right_symbol: str = RIGHT_SYMBOL
+
+    @property
+    def all_placeholders(self) -> list[str]:
+        """Return the list of placeholder tokens for this template."""
+        token_list = [self.asset_name]
+        if self.max_idx:
+            token_list.extend([f"{self.asset_name}_{idx}" for idx in range(1, self.max_idx + 1)])
+
+        return [f"{self.left_symbol}{token}{self.right_symbol}" for token in token_list]
+
+    @property
+    def all_added_tokens(self) -> list[AddedToken]:
+        """Return a list of all placeholders as `AddedToken`s."""
+        added_token_list = [
+            AddedToken(token, single_word=True, lstrip=False, rstrip=False, normalized=True)
+            for token in self.all_placeholders
+        ]
+        return added_token_list
+
+
+PLACEHOLDER_TOKENS = (
+    AssetPlaceholderToken(asset_name="base_obj", max_idx=2),
+    AssetPlaceholderToken(asset_name="dragged_obj", max_idx=5),
+    AssetPlaceholderToken(asset_name="swept_obj"),
+    AssetPlaceholderToken(asset_name="bounds"),
+    AssetPlaceholderToken(asset_name="constraint"),
+    AssetPlaceholderToken(asset_name="scene"),
+    AssetPlaceholderToken(asset_name="demo_blicker_obj", max_idx=3),
+    AssetPlaceholderToken(asset_name="demo_less_blicker_obj", max_idx=3),
+    AssetPlaceholderToken(asset_name="start_scene", max_idx=3),
+    AssetPlaceholderToken(asset_name="end_scene", max_idx=3),
+    AssetPlaceholderToken(asset_name="before_twist", max_idx=3),
+    AssetPlaceholderToken(asset_name="after_twist", max_idx=3),
+    AssetPlaceholderToken(asset_name="frame", max_idx=6),
+    AssetPlaceholderToken(asset_name="ring"),
+    AssetPlaceholderToken(asset_name="hanoi_stand"),
+)
+
+
+def get_placeholders_list(
+    placeholder_tokens: Iterable[AssetPlaceholderToken] = PLACEHOLDER_TOKENS,
+) -> set[str]:
+    """Get a list of all the placeholders used from the tokens."""
+    placeholders: set[str] = set()
+    for token in placeholder_tokens:
+        placeholders.update(token.all_placeholders)
+    return placeholders
+
+
+def add_placeholder_tokens_to_tokenizer(
+    tokenizer: Tokenizer, placeholder_tokens: Iterable[AssetPlaceholderToken] = PLACEHOLDER_TOKENS
+) -> None:
+    """Add the placeholder tokens into the tokenizer.
+
+    This is non-pure and will alter the tokenizer given in place.
+    """
+    for token in placeholder_tokens:
+        tokenizer.add_tokens(token.all_added_tokens)
