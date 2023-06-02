e61c347bb6b62954e336fabb832e5a8e961318de
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-06-02 16:08:25 +0100

    feat(vima): create module for `prepare_prompt` function from `scripts/example.py`

---
+++ src/vima/prepare_prompt.py
@@ -0,0 +1,154 @@
+from typing import TYPE_CHECKING
+
+import cv2
+import numpy as np
+from einops import rearrange
+
+from vima.utils import (
+    any_concat,
+    any_stack,
+    any_to_datadict,
+    any_to_torch_tensor,
+    stack_sequence_fields,
+)
+
+
+if TYPE_CHECKING:
+    import torch
+    from tokenizers import Tokenizer
+
+
+def prepare_prompt(
+    *,
+    prompt: str,
+    prompt_assets: dict,
+    views: list[str],
+    tokenizer: Tokenizer,
+    placeholders: list[str],
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Prepare the promot from the assets and the prompt string.
+
+    This is taken from `vima/scripts/example.py:prepare_prompt`.
+    """
+    views = sorted(views)
+    encoding = tokenizer.encode(prompt, add_special_tokens=True)
+    prompt_ids, prompt_tokens = encoding.ids, encoding.tokens
+    assert set(prompt_assets.keys()) == {
+        token[1:-1] for token in prompt_tokens if token in placeholders
+    }
+    filled_prompt = []
+    for id, token in zip(prompt_ids, prompt_tokens):
+        if token not in placeholders:
+            assert "{" not in token
+            assert "}" not in token
+            filled_prompt.append(id)
+        else:
+            assert token.startswith("{")
+            assert token.endswith("}")
+            asset_name = token[1:-1]
+            assert asset_name in prompt_assets, f"missing prompt asset {asset_name}"
+            asset = prompt_assets[asset_name]
+            obj_info = asset["segm"]["obj_info"]
+            placeholder_type = asset["placeholder_type"]
+            if placeholder_type == "object":
+                objects = [obj_info["obj_id"]]
+            elif placeholder_type == "scene":
+                objects = [each_info["obj_id"] for each_info in obj_info]
+            obj_repr = {
+                "cropped_img": {view: [] for view in views},
+                "bbox": {view: [] for view in views},
+            }
+            for view in views:
+                rgb_this_view = asset["rgb"][view]
+                segm_this_view = asset["segm"][view]
+                bboxes = []
+                cropped_imgs = []
+                for obj_id in objects:
+                    ys, xs = np.nonzero(segm_this_view == obj_id)
+                    if len(xs) < 2 or len(ys) < 2:
+                        continue
+                    xmin, xmax = np.min(xs), np.max(xs)
+                    ymin, ymax = np.min(ys), np.max(ys)
+                    x_center, y_center = (xmin + xmax) / 2, (ymin + ymax) / 2
+                    h, w = ymax - ymin, xmax - xmin
+                    bboxes.append([int(x_center), int(y_center), int(h), int(w)])
+                    cropped_img = rgb_this_view[:, ymin : ymax + 1, xmin : xmax + 1]
+                    if cropped_img.shape[1] != cropped_img.shape[2]:
+                        diff = abs(cropped_img.shape[1] - cropped_img.shape[2])
+                        pad_before, pad_after = int(diff / 2), diff - int(diff / 2)
+                        if cropped_img.shape[1] > cropped_img.shape[2]:
+                            pad_width = ((0, 0), (0, 0), (pad_before, pad_after))
+                        else:
+                            pad_width = ((0, 0), (pad_before, pad_after), (0, 0))
+                        cropped_img = np.pad(
+                            cropped_img,
+                            pad_width,
+                            mode="constant",
+                            constant_values=0,
+                        )
+                        assert cropped_img.shape[1] == cropped_img.shape[2], "INTERNAL"
+                    cropped_img = rearrange(cropped_img, "c h w -> h w c")
+                    cropped_img = np.asarray(cropped_img)
+                    cropped_img = cv2.resize(
+                        cropped_img,
+                        (32, 32),
+                        interpolation=cv2.INTER_AREA,
+                    )
+                    cropped_img = rearrange(cropped_img, "h w c -> c h w")
+                    cropped_imgs.append(cropped_img)
+                bboxes = np.asarray(bboxes)
+                cropped_imgs = np.asarray(cropped_imgs)
+                obj_repr["bbox"][view] = bboxes
+                obj_repr["cropped_img"][view] = cropped_imgs
+            filled_prompt.append(obj_repr)
+    raw_prompt = [filled_prompt]
+    max_n_objs_prompt = {view: 0 for view in views}
+    for prompt in raw_prompt:
+        for token in prompt:
+            if isinstance(token, dict):
+                for view in views:
+                    max_n_objs_prompt[view] = max(
+                        max_n_objs_prompt[view], len(token["cropped_img"][view])
+                    )
+    raw_prompt_token_type, word_batch, image_batch = [], [], []
+    for prompt in raw_prompt:
+        token_type = []
+        for token in prompt:
+            if isinstance(token, int):
+                token_type.append(0)
+                word_batch.append(token)
+            elif isinstance(token, dict):
+                token_type.append(1)
+                n_objs_prompt = {view: len(token["cropped_img"][view]) for view in views}
+                # add mask
+                token["mask"] = {
+                    view: np.ones((n_objs_prompt[view],), dtype=bool) for view in views
+                }
+                n_objs_to_pad = {
+                    view: max_n_objs_prompt[view] - n_objs_prompt[view] for view in views
+                }
+                objs_pad = {
+                    "bbox": {
+                        view: np.zeros((n_objs_to_pad[view], 4), dtype=np.int64) for view in views
+                    },
+                    "cropped_img": {
+                        view: np.zeros(
+                            (n_objs_to_pad[view], 3, 32, 32),
+                            dtype=np.uint8,
+                        )
+                        for view in views
+                    },
+                    "mask": {view: np.zeros((n_objs_to_pad[view]), dtype=bool) for view in views},
+                }
+                token = any_concat([token, objs_pad], dim=0)
+                image_batch.append(token)
+        raw_prompt_token_type.append(token_type)
+    assert sum([len(prompt) for prompt in raw_prompt_token_type]) == len(word_batch) + len(
+        image_batch
+    )
+    word_batch = any_stack(word_batch, dim=0)
+    image_batch = any_to_datadict(stack_sequence_fields(image_batch))
+
+    word_batch = any_to_torch_tensor(word_batch)
+    image_batch = image_batch.to_torch_tensor()
+    return raw_prompt_token_type, word_batch, image_batch
