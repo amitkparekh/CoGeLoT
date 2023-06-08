5dabd9aba06faa0eed2a8ded8e766c48c31f0fc7
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-06-08 10:02:53 +0100

    refactor: move end effector to a separate constants module

---
+++ src/cogelot/data/constants.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from typing import Literal
+
+
+EndEffector = Literal["suction", "spatula"]
--- src/cogelot/modules/tokenizers/end_effector.py
+++ src/cogelot/modules/tokenizers/end_effector.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
+from typing import get_args
+
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
+
+from cogelot.data.constants import EndEffector
 
 
 UNK_TOKEN = "[UNK]"  # noqa: S105
 
-END_EFFECTOR_VOCAB = {
-    "suction": 0,
-    "spatula": 1,
-}
+END_EFFECTOR_VOCAB = {end_effector: idx for idx, end_effector in enumerate(get_args(EndEffector))}
 
 
 def create_end_effector_tokenizer(
