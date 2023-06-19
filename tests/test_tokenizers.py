5ecc0a27d7b3754da7aa2f0a572867e7e4105e16
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-06-19 16:32:34 +0100

    test(tokenizer): add tests for pose action tokenizer

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -1,4 +1,4 @@
-from cogelot.modules.tokenizers import ImageTokenizer, TextTokenizer
+from cogelot.modules.tokenizers import ImageTokenizer, PoseActionTokenizer, TextTokenizer
 from cogelot.structures.vima import VIMAInstance
 
 
@@ -19,3 +19,16 @@ def test_image_tokenizer_does_not_fail(
         for observation in normalized_instance.observations
     ]
     assert tokens is not None
+
+
+def test_pose_action_tokenizer_works_properly(
+    normalized_instance: VIMAInstance, pose_action_tokenizer: PoseActionTokenizer
+) -> None:
+    tokenized_actions = pose_action_tokenizer.tokenize(normalized_instance.pose_actions)
+
+    assert tokenized_actions
+
+    # Make sure each is discrete and not a floating point
+    for token in tokenized_actions:
+        for tensor in token.to_target_pose_action().values():
+            assert not tensor.is_floating_point()
