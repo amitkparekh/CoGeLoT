912b585f542207a0900189b489eedb9fe1fd756d
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-06-19 16:36:43 +0100

    test(fixtures): create more modules in the fixtures

--- tests/fixtures/modules.py
+++ tests/fixtures/modules.py
@@ -1,8 +1,8 @@
-from __future__ import annotations
-
 from pytest_cases import fixture
 
 from cogelot.data.preprocess import InstancePreprocessor
+from cogelot.models.vima import VIMALightningModule
+from cogelot.modules.preprocessors.their_instance_batcher import TheirInstanceBatcher
 from cogelot.modules.tokenizers import (
     EndEffectorTokenizer,
     ImageTokenizer,
@@ -13,6 +13,7 @@ from cogelot.modules.tokenizers import (
     TextTokenizer,
 )
 from cogelot.structures.common import View
+from vima.policy import VIMAPolicy
 
 
 @fixture(scope="session")
@@ -50,18 +51,50 @@ def observation_tokenizer(
 
 
 @fixture(scope="session")
-def instance_preprocessor(
-    text_tokenizer: TextTokenizer,
-    image_tokenizer: ImageTokenizer,
-    pose_action_tokenizer: PoseActionTokenizer,
+def multimodal_prompt_tokenizer(
+    text_tokenizer: TextTokenizer, image_tokenizer: ImageTokenizer
+) -> MultimodalPromptTokenizer:
+    return MultimodalPromptTokenizer(
+        text_tokenizer=text_tokenizer,
+        image_tokenizer=image_tokenizer,
+        views=[View.front, View.top],
+    )
+
+
+@fixture(scope="session")
+def multimodal_history_tokenizer(
     observation_tokenizer: ObservationTokenizer,
-) -> InstancePreprocessor:
-    return InstancePreprocessor(
-        multimodal_prompt_tokenizer=MultimodalPromptTokenizer(
-            text_tokenizer, image_tokenizer, list(View)
-        ),
-        multimodal_history_tokenizer=MultimodalHistoryTokenizer(
-            observation_tokenizer, pose_action_tokenizer
-        ),
+    pose_action_tokenizer: PoseActionTokenizer,
+) -> MultimodalHistoryTokenizer:
+    return MultimodalHistoryTokenizer(
+        observation_tokenizer=observation_tokenizer,
         pose_action_tokenizer=pose_action_tokenizer,
     )
+
+
+@fixture(scope="session")
+def instance_preprocessor(
+    text_tokenizer: TextTokenizer,
+    end_effector_tokenizer: EndEffectorTokenizer,
+    pose_action_tokenizer: PoseActionTokenizer,
+) -> InstancePreprocessor:
+    return InstancePreprocessor(
+        text_tokenizer=text_tokenizer,
+        end_effector_tokenizer=end_effector_tokenizer,
+        pose_action_tokenizer=pose_action_tokenizer,
+    )
+
+
+@fixture(scope="session")
+def vima_policy() -> VIMAPolicy:
+    return VIMAPolicy(embed_dim=768, xf_n_layers=2, sattn_n_heads=2, xattn_n_heads=2)
+
+
+@fixture(scope="session")
+def their_instance_batcher(vima_policy: VIMAPolicy) -> TheirInstanceBatcher:
+    return TheirInstanceBatcher(vima_policy=vima_policy)
+
+
+@fixture(scope="session")
+def vima_lightning_module(vima_policy: VIMAPolicy) -> VIMALightningModule:
+    return VIMALightningModule(vima_policy=vima_policy)
