957bf68753f3bb156844b17894b7c1d12d09abe6
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-06-19 16:40:38 +0100

    refactor(vima/action_decoder/dists): make private attributes public

--- src/vima/nn/action_decoder/dists.py
+++ src/vima/nn/action_decoder/dists.py
@@ -13,13 +13,13 @@ class MultiCategorical(torch.distributions.Distribution):
     def __init__(self, logits, action_dims: list[int]) -> None:
         assert logits.dim() >= 2, logits.shape
         super().__init__(batch_shape=logits[:-1], validate_args=False)
-        self._action_dims = tuple(action_dims)
+        self.action_dims = tuple(action_dims)
         assert logits.size(-1) == sum(
-            self._action_dims
-        ), f"sum of action dims {self._action_dims} != {logits.size(-1)}"
-        self._dists = [
+            self.action_dims
+        ), f"sum of action dims {self.action_dims} != {logits.size(-1)}"
+        self.dists = [
             Categorical(logits=split) for split in torch.split(logits, action_dims, dim=-1)
         ]
 
     def mode(self):
-        return torch.stack([torch.argmax(dist.probs, dim=-1) for dist in self._dists], dim=-1)
+        return torch.stack([torch.argmax(dist.probs, dim=-1) for dist in self.dists], dim=-1)
