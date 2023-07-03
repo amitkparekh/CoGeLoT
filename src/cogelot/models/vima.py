e67304773b5f8c31fdba7da6ca0d23e7633a6ee2
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-07-03 15:03:48 +0100

    feat(metrics): add torchmetric trackers for loss and accuracy

--- src/cogelot/models/vima.py
+++ src/cogelot/models/vima.py
@@ -2,6 +2,8 @@ from typing import TYPE_CHECKING, cast, get_args
 
 import torch
 from lightning import pytorch as pl
+from torchmetrics import MeanMetric
+from torchmetrics.classification import MulticlassAccuracy
 
 from cogelot.modules.preprocessors.their_instance_batcher import (
     TheirInstanceBatcher,
@@ -20,13 +22,16 @@ if TYPE_CHECKING:
 class VIMALightningModule(pl.LightningModule):
     """Lighting module for a VIMA model."""
 
+    ignore_target_index: int = -100
+
     def __init__(self, *, vima_policy: VIMAPolicy) -> None:
         super().__init__()
 
         self.vima_policy = vima_policy
         self.instance_batcher = TheirInstanceBatcher(vima_policy)
 
-        self.loss = torch.nn.CrossEntropyLoss()
+        self.loss = MeanMetric()
+        self.accuracy = MulticlassAccuracy(num_classes=100, ignore_index=self.ignore_target_index)
 
     def forward(
         self, instances: list[PreprocessedInstance]
@@ -86,8 +91,21 @@ class VIMALightningModule(pl.LightningModule):
             for target_axis, predicted_dist_axis in iterator:
                 predicted_logits: torch.Tensor = cast(torch.Tensor, predicted_dist_axis.logits)
                 predicted_logits = predicted_logits.reshape(-1, predicted_logits.shape[-1])
-                losses.append(self.loss(predicted_logits, target_axis.reshape(-1)))
+                losses.append(
+                    torch.nn.functional.cross_entropy(
+                        predicted_logits,
+                        target_axis.reshape(-1),
+                        ignore_index=self.ignore_target_index,
+                    )
+                )
+                self.accuracy.update(predicted_logits.argmax(-1), target_axis.reshape(-1))
 
-        # Average all the losses
-        average_loss = torch.mean(torch.stack(losses))
+        # Convert loss list to tensor
+        stacked_loss = torch.stack(losses)
+
+        # Update the loss tracker for the epoch
+        self.loss.update(stacked_loss)
+
+        # Return the average loss for the batch
+        average_loss = torch.mean(stacked_loss)
         return average_loss
