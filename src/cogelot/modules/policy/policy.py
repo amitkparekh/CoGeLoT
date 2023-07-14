333b269f66bc001047edfacf5bd8391ce7e3b5cf
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-07-14 13:08:47 +0100

    feat!: migrate their policy into our interface
    this is so that things are easier to iterate with

--- src/cogelot/modules/policy/policy.py
+++ src/cogelot/modules/policy/policy.py
@@ -1,11 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import ClassVar, cast
 
 import torch
 
 from cogelot.structures.model import RawPromptTokenType
 from cogelot.structures.vima import PoseActionType
+from vima import nn as vnn
 from vima.nn.action_decoder.dists import MultiCategorical
 from vima.utils import DataDict
+
+
+def get_max_length_of_prompt(raw_prompts_token_type: RawPromptTokenType, max_num_objs: int) -> int:
+    """Get the maximum length of the prompt.
+
+    Refactored by ChatGPT to be more efficient and readable.
+    """
+    max_length = 0
+
+    for raw_prompt in raw_prompts_token_type:
+        current_length = sum([1 if token_type == 0 else max_num_objs for token_type in raw_prompt])
+        max_length = max(max_length, current_length)
+
+    return max_length
 
 
 class Policy(ABC, torch.nn.Module):
@@ -18,47 +34,37 @@ class Policy(ABC, torch.nn.Module):
     n_discrete_y_bins: int = 100
     n_discrete_z_bins: int = 50
     n_discrete_rot_bins: int = 50
+    _views: ClassVar[list[str]] = ["front", "top"]
 
-    def forward(
+    def __init__(
         self,
-        encoded_prompt: torch.Tensor,
-        encoded_prompt_mask: torch.Tensor,
-        embedded_observations: torch.Tensor,
-        embedded_observations_mask: torch.Tensor,
-        embedded_actions: torch.Tensor | None,
-    ) -> torch.Tensor:
-        """Predict the action token."""
-        return self.predict_action_token(
-            encoded_prompt=encoded_prompt,
-            encoded_prompt_mask=encoded_prompt_mask,
-            embedded_observations=embedded_observations,
-            embedded_observations_mask=embedded_observations_mask,
-            embedded_actions=embedded_actions,
+        *,
+        embed_dim: int,
+        obj_encoder: vnn.ObjEncoder,
+        end_effector_encoder: vnn.Embedding,
+        obs_fusion_layer: torch.nn.Linear,
+        action_encoder: vnn.ActionEmbedding,
+        action_decoder: vnn.ActionDecoder,
+        prompt_embedding: vnn.WordEmbedding,
+        prompt_encoder: vnn.T5PromptEncoder,
+        prompt_obj_post_layer: torch.nn.Sequential,
+    ) -> None:
+        super().__init__()
+        self.embed_dim = embed_dim
+
+        self._obj_encoder = obj_encoder
+        self._end_effector_encoder = end_effector_encoder
+        self._obs_fusion_layer = obs_fusion_layer
+        self._action_encoder = action_encoder
+        self._action_decoder = action_decoder
+        self._prompt_embedding = prompt_embedding
+        self._prompt_encoder = prompt_encoder
+        self._prompt_encoder_post_layer = (
+            torch.nn.Identity()
+            if embed_dim == self._prompt_encoder.output_dim
+            else torch.nn.Linear(self._prompt_encoder.output_dim, embed_dim, bias=False)
         )
-
-    @abstractmethod
-    def assemble_prompt(
-        self, prompts: tuple[RawPromptTokenType, torch.Tensor, DataDict]
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        """Assemble and embed prompts."""
-        ...  # noqa: WPS428
-
-    @abstractmethod
-    def embed_observation_token(self, observation: DataDict) -> tuple[torch.Tensor, torch.Tensor]:
-        """Embed observations for an instance."""
-        ...  # noqa: WPS428
-
-    @abstractmethod
-    def embed_action_token(self, actions: DataDict) -> torch.Tensor:
-        """Embed the actions into a tensor."""
-        ...  # noqa: WPS428
-
-    @abstractmethod
-    def encode_prompt(
-        self, embedded_prompt: torch.Tensor, embedded_prompt_mask: torch.Tensor
-    ) -> torch.Tensor:
-        """Encode the prompt."""
-        ...  # noqa: WPS428
+        self._prompt_obj_post_layer = prompt_obj_post_layer
 
     @abstractmethod
     def predict_action_token(
@@ -72,12 +78,140 @@ class Policy(ABC, torch.nn.Module):
         """Predict the action token."""
         ...  # noqa: WPS428
 
-    @abstractmethod
+    def assemble_prompt(  # noqa: WPS210
+        self, prompts: tuple[RawPromptTokenType, torch.Tensor, DataDict]
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Assemble and embed the prompt.
+
+        Taken from vima.policy.VIMAPolicy. Refactored with ChatGPT to be more efficient and
+        readable.
+        """
+        raw_prompts_token_type, word_batch, image_batch = prompts
+        batch_word_emb = self._prompt_embedding(word_batch)
+        batch_image_emb = self._obj_encoder(**image_batch)
+        batch_image_emb = self._prompt_obj_post_layer(batch_image_emb)
+
+        max_num_objs: int = batch_image_emb.shape[-2]
+        max_prompt_length = get_max_length_of_prompt(raw_prompts_token_type, max_num_objs)
+
+        prompt_tokens: list[torch.Tensor] = []
+        prompt_masks: list[torch.Tensor] = []
+        word_idx, img_idx = 0, 0
+
+        for raw_prompt_tokens in raw_prompts_token_type:
+            assembled_prompt: list[torch.Tensor] = []
+            assembled_mask: list[bool] = []
+
+            for raw_prompt_token in raw_prompt_tokens:
+                # If 0, means that the token is a word
+                if raw_prompt_token == 0:
+                    word_idx += 1
+                    assembled_prompt.append(batch_word_emb[word_idx])
+                    assembled_mask.append(True)
+
+                # If 1, means that the token is an image
+                if raw_prompt_token == 1:
+                    img_idx += 1
+                    obj_masks: list[bool] = [
+                        image_batch["mask"][view][  # pyright: ignore[reportGeneralTypeIssues]
+                            img_idx
+                        ]
+                        for view in sorted(self._views)
+                    ]
+                    obj_embeddings = batch_image_emb[img_idx][:max_num_objs]
+                    assembled_prompt.extend(obj_embeddings)
+                    assembled_mask.extend(obj_masks[:max_num_objs])
+
+            additional_padding_for_prompt = max_prompt_length - len(assembled_prompt)
+
+            padding = torch.zeros(
+                size=(additional_padding_for_prompt, assembled_prompt[0].shape[0]),
+                dtype=torch.float32,
+                device=assembled_prompt[0].device,
+            )
+            token_tensor_for_prompt = torch.stack(tensors=[*assembled_prompt, padding], dim=0)
+            prompt_tokens.append(token_tensor_for_prompt)
+
+            prompt_masks.append(
+                torch.cat(
+                    [
+                        torch.tensor(
+                            assembled_mask,
+                            dtype=torch.bool,
+                            device=token_tensor_for_prompt.device,
+                        ),
+                        torch.zeros(
+                            additional_padding_for_prompt,
+                            dtype=torch.bool,
+                            device=token_tensor_for_prompt.device,
+                        ),
+                    ],
+                    dim=0,
+                )
+            )
+
+        prompt_tokens_tensor = torch.stack(prompt_tokens, dim=0)
+        prompt_masks_tensor = torch.stack(prompt_masks, dim=0)
+        return prompt_tokens_tensor, prompt_masks_tensor
+
+    def embed_observation_token(self, observation: DataDict) -> tuple[torch.Tensor, torch.Tensor]:
+        """Embed an observation."""
+        obs_objects, ee = observation["objects"], observation["ee"]
+
+        assert isinstance(obs_objects, DataDict)
+        assert isinstance(ee, torch.Tensor)
+
+        leading_dims = ee.shape[:2]
+
+        # Get the features for each image/obj/obs
+        obs_objects = obs_objects.map_structure(
+            func=lambda x: x.reshape(-1, *x.shape[2:]),
+        )
+        # I know the type from manual inspection
+        obs_objects = cast(dict[str, dict[str, torch.Tensor]], obs_objects)
+        img_feats = self._obj_encoder(**obs_objects)
+        img_feats = img_feats.reshape(*leading_dims, *img_feats.shape[1:])
+
+        # Get EE features
+        ee_feats: torch.Tensor = self._end_effector_encoder(ee)
+        ee_feats = ee_feats.unsqueeze(2).repeat(1, 1, img_feats.shape[-2], 1)  # noqa: WPS221
+
+        # Create obs features
+        obs_feats: torch.Tensor = self._obs_fusion_layer(torch.cat([img_feats, ee_feats], dim=-1))
+
+        # Create mask for obs
+        obj_mask = {
+            obj_key: obs_objects["mask"][obj_key].reshape(*leading_dims, -1)
+            for obj_key in obs_objects["mask"]
+        }
+        obj_mask_tensor = torch.cat(
+            [obj_mask[view] for view in sorted(self._views)],
+            dim=-1,
+        )
+
+        return obs_feats, obj_mask_tensor
+
+    def embed_action_token(self, actions: DataDict) -> torch.Tensor:
+        """Embed the actions into a tensor."""
+        return self._action_encoder(
+            self.de_discretize_actions(cast(dict[PoseActionType, torch.Tensor], actions))
+        )
+
+    def encode_prompt(
+        self, embedded_prompt: torch.Tensor, embedded_prompt_mask: torch.Tensor
+    ) -> torch.Tensor:
+        """Encode the prompt."""
+        prompt_tokens = self._prompt_encoder(
+            embedded_prompt, attention_mask=embedded_prompt_mask, batch_first=True
+        )
+        prompt_tokens = self._prompt_encoder_post_layer(prompt_tokens)
+        return prompt_tokens
+
     def decode_action_token(
         self, predicted_action_tokens: torch.Tensor
     ) -> dict[PoseActionType, MultiCategorical]:
         """Decode the action token."""
-        ...  # noqa: WPS428
+        return self._action_decoder(predicted_action_tokens)
 
     def discretize_action(
         self, action: dict[PoseActionType, torch.Tensor]
--- src/cogelot/modules/policy/vima.py
+++ src/cogelot/modules/policy/vima.py
@@ -1,39 +1,44 @@
-from typing import cast
-
 import torch
+from einops import rearrange
 
 from cogelot.modules.policy.policy import Policy
-from cogelot.structures.model import RawPromptTokenType
-from cogelot.structures.vima import PoseActionType
-from vima.nn.action_decoder.dists import MultiCategorical
-from vima.policy import VIMAPolicy as TheirPolicy
-from vima.utils import DataDict
+from vima import nn as vnn
 
 
-class VIMAPolicy(Policy, TheirPolicy):
-    """Wrap their policy in our interface."""
+class VIMAPolicy(Policy):
+    """Put their policy in our interface.
 
-    def assemble_prompt(
-        self, prompts: tuple[RawPromptTokenType, torch.Tensor, DataDict]
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        """Assemble and embed the prompt."""
-        return TheirPolicy.forward_prompt_assembly(self, prompts)
+    Refactored version of their policy. Is functionally, and essentially, identical to their
+    policy.
+    """
 
-    def encode_prompt(
-        self, embedded_prompt: torch.Tensor, embedded_prompt_mask: torch.Tensor
-    ) -> torch.Tensor:
-        """Encode the prompt."""
-        return TheirPolicy.forward_prepared_prompt(self, embedded_prompt, embedded_prompt_mask)
-
-    def embed_observation_token(self, observation: DataDict) -> tuple[torch.Tensor, torch.Tensor]:
-        """Embed an observation."""
-        return cast(
-            tuple[torch.Tensor, torch.Tensor], TheirPolicy.forward_obs_token(self, observation)
+    def __init__(
+        self,
+        *,
+        embed_dim: int,
+        obj_encoder: vnn.ObjEncoder,
+        end_effector_encoder: vnn.Embedding,
+        obs_fusion_layer: torch.nn.Linear,
+        action_encoder: vnn.ActionEmbedding,
+        action_decoder: vnn.ActionDecoder,
+        prompt_embedding: vnn.WordEmbedding,
+        prompt_encoder: vnn.T5PromptEncoder,
+        prompt_obj_post_layer: torch.nn.Sequential,
+        xattn_gpt: vnn.XAttnGPT,
+    ) -> None:
+        super().__init__(
+            embed_dim=embed_dim,
+            obj_encoder=obj_encoder,
+            end_effector_encoder=end_effector_encoder,
+            obs_fusion_layer=obs_fusion_layer,
+            action_encoder=action_encoder,
+            action_decoder=action_decoder,
+            prompt_embedding=prompt_embedding,
+            prompt_encoder=prompt_encoder,
+            prompt_obj_post_layer=prompt_obj_post_layer,
         )
 
-    def embed_action_token(self, actions: DataDict) -> torch.Tensor:
-        """Embed the actions into a tensor."""
-        return TheirPolicy.forward_action_token(self, actions)
+        self._xattn_gpt = xattn_gpt
 
     def predict_action_token(
         self,
@@ -44,8 +49,7 @@ class VIMAPolicy(Policy, TheirPolicy):
         embedded_actions: torch.Tensor | None,
     ) -> torch.Tensor:
         """Predict the next action."""
-        return TheirPolicy.forward(
-            self,
+        return self._their_forward(
             obs_token=embedded_observations,
             obs_mask=embedded_observations_mask,
             prompt_token=encoded_prompt,
@@ -53,8 +57,54 @@ class VIMAPolicy(Policy, TheirPolicy):
             action_token=embedded_actions,
         )
 
-    def decode_action_token(
-        self, predicted_action_tokens: torch.Tensor
-    ) -> dict[PoseActionType, MultiCategorical]:
-        """Decode the action token."""
-        return TheirPolicy.forward_action_decoder(self, predicted_action_tokens)
+    def _their_forward(  # noqa: WPS210
+        self,
+        obs_token: torch.Tensor,
+        obs_mask: torch.Tensor,
+        action_token: torch.Tensor | None,
+        prompt_token: torch.Tensor,
+        prompt_token_mask: torch.Tensor,
+    ) -> torch.Tensor:
+        # Rearrange the tensors to be in the right structure
+        # obs_token = rearrange(obs_token, "L B Q E -> B L Q E")
+        obs_token = rearrange(obs_token, "B L Q E -> B (L Q) E")
+        obs_token = rearrange(obs_token, "B L E -> L B E")
+
+        # obs_mask = rearrange(obs_mask, "L B Q -> B L Q")
+        obs_mask = rearrange(obs_mask, "B L Q -> B (L Q)")
+        obs_mask = rearrange(obs_mask, "B L -> L B")
+
+        # Create tensors which will we will use to put the various tokens into
+        batch_size, observation_seq_len = obs_token.shape[:2]
+        actions_seq_len = 0 if action_token is None else action_token.shape[1]
+        max_objects = obs_token.shape[-2]
+        total_seq_len = observation_seq_len * max_objects + actions_seq_len
+
+        tokens = torch.empty(
+            total_seq_len, batch_size, self.embed_dim, dtype=torch.float32, device=obs_token.device
+        )
+        masks = torch.ones(total_seq_len, batch_size, dtype=torch.bool, device=obs_token.device)
+
+        # Fill in the tokens and masks properly
+        for obj_idx in range(max_objects):
+            tokens[obj_idx :: max_objects + 1] = obs_token[obj_idx::max_objects]  # noqa: WPS362
+            masks[obj_idx :: max_objects + 1] = obs_mask[obj_idx::max_objects]  # noqa: WPS362
+
+        if action_token is not None:
+            tokens[max_objects :: max_objects + 1] = action_token.transpose(0, 1)  # noqa: WPS362
+
+        position_ids = torch.cumsum(masks, dim=0) - 1
+        position_ids = position_ids.long()
+        prompt_position_ids = torch.cumsum(prompt_token_mask, dim=1) - 1
+
+        tokens_out = self._xattn_gpt(
+            obs_action_tokens=tokens,
+            prompt_tokens=prompt_token.transpose(0, 1),
+            prompt_mask=prompt_token_mask,
+            obs_action_masks=masks.transpose(0, 1),
+            obs_action_position_ids=position_ids.transpose(0, 1),
+            prompt_position_ids=prompt_position_ids,
+        )
+
+        predicted_action_tokens = tokens_out[max_objects - 1 :: max_objects + 1]
+        return predicted_action_tokens
