c77efcb9b1c28b148d8f84446476b3f423945054
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-07-19 15:19:16 +0100

    feat(policy): add policy's using x-transformers
    if this works, that's alibi sorted too

--- src/cogelot/nn/decoders/__init__.py
+++ src/cogelot/nn/decoders/__init__.py
@@ -4,3 +4,4 @@ from cogelot.nn.decoders.interfaces import (
 )
 from cogelot.nn.decoders.policy import PolicyTransformerDecoder
 from cogelot.nn.decoders.vima import VIMATransformerDecoder
+from cogelot.nn.decoders.x_transformer import XTransformerDecoder
