58f627374a45557a0dc079d13772432ea5b580f5
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-07-19 15:29:34 +0100

    fix: remove `PolicyTransformerDecoder` from init

--- src/cogelot/nn/decoders/__init__.py
+++ src/cogelot/nn/decoders/__init__.py
@@ -2,6 +2,5 @@ from cogelot.nn.decoders.interfaces import (
     TransformerDecoderLayerProtocol,
     TransformerDecoderProtocol,
 )
-from cogelot.nn.decoders.policy import PolicyTransformerDecoder
 from cogelot.nn.decoders.vima import VIMATransformerDecoder
 from cogelot.nn.decoders.x_transformer import XTransformerDecoder
