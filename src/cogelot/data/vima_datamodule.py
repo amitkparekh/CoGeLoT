44e93e2340733d8524e9c240b2f88686f2c13116
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-01 14:32:15 +0100

    feat: support dataset streaming and node splitting

--- src/cogelot/data/vima_datamodule.py
+++ src/cogelot/data/vima_datamodule.py
@@ -5,7 +5,7 @@ from lightning import LightningDataModule
 from torch.utils.data import DataLoader
 
 from cogelot.data.collate import collate_preprocessed_instances_from_hf_dataset
-from cogelot.data.datasets import set_dataset_format
+from cogelot.data.datasets import maybe_split_dataset_by_node
 from cogelot.structures.model import PreprocessedInstance
 
 
@@ -21,6 +21,7 @@ class VIMADataModule(LightningDataModule):
         hf_datasets_repo_name: str,
         num_workers: int,
         batch_size: int,
+        use_dataset_streaming: bool = False,
         dataloader_kwargs: dict[str, Any] | None = None
     ) -> None:
         super().__init__()
@@ -29,6 +30,11 @@ class VIMADataModule(LightningDataModule):
         self._dataloader_kwargs = dataloader_kwargs or {}
 
         self.batch_size = batch_size
+        self._use_dataset_streaming = use_dataset_streaming
+
+        # When using dataset streaming, we need to disable shuffling.
+        if use_dataset_streaming:
+            self._dataloader_kwargs["shuffle"] = False
 
         self.train_dataset: datasets.Dataset
         self.valid_dataset: datasets.Dataset
@@ -36,28 +42,22 @@ class VIMADataModule(LightningDataModule):
     def prepare_data(self) -> None:
         """Prepare any data before starting training.
 
-        This is just making sure the dataset has already been downloaded.
+        This is just making sure the dataset has already been downloaded. If we are using
+        streaming, we don't need to do anything here.
         """
-        datasets.load_dataset(self._hf_datasets_repo_name, num_proc=self._num_workers)
+        if not self._use_dataset_streaming:
+            datasets.load_dataset(self._hf_datasets_repo_name, num_proc=self._num_workers)
 
     def setup(self, stage: SetupStage) -> None:
         """Setup each GPU to run the data."""
         if stage == "fit":
-            dataset = datasets.load_dataset(
-                self._hf_datasets_repo_name, num_proc=self._num_workers
-            )
-            assert isinstance(dataset, datasets.DatasetDict)
-            dataset = set_dataset_format(dataset)
-            self.train_dataset = dataset["train"]
-            self.valid_dataset = dataset["valid"]
+            dataset = self._load_dataset()
+            self.train_dataset = maybe_split_dataset_by_node(dataset["train"])
+            self.valid_dataset = maybe_split_dataset_by_node(dataset["valid"])
 
         if stage == "validate":
-            dataset = datasets.load_dataset(
-                self._hf_datasets_repo_name, split="valid", num_proc=self._num_workers
-            )
-            assert isinstance(dataset, datasets.DatasetDict)
-            dataset = set_dataset_format(dataset)
-            self.valid_dataset = dataset["valid"]
+            dataset = self._load_dataset()
+            self.valid_dataset = maybe_split_dataset_by_node(dataset["valid"])
 
     def train_dataloader(self) -> DataLoader[list[PreprocessedInstance]]:
         """Create the dataloader for the training set."""
@@ -80,3 +80,12 @@ class VIMADataModule(LightningDataModule):
             collate_fn=collate_preprocessed_instances_from_hf_dataset,
             **self._dataloader_kwargs,
         )
+
+    def _load_dataset(self) -> datasets.DatasetDict | datasets.IterableDatasetDict:
+        """Load the dataset from HF."""
+        dataset = datasets.load_dataset(
+            self._hf_datasets_repo_name,
+            streaming=self._use_dataset_streaming,
+        )
+        assert isinstance(dataset, datasets.DatasetDict | datasets.IterableDatasetDict)
+        return dataset
