1f23faa21f2fad26013dd3f9d2c0c315624ab999
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-16 09:26:16 +0100

    refactor: decouple methods which are essentially functions from the policy

--- src/cogelot/modules/policy.py
+++ src/cogelot/modules/policy.py
@@ -6,7 +6,10 @@ from einops import rearrange
 
 from cogelot.nn.decoders import TransformerDecoderProtocol
 from cogelot.structures.model import RawPromptTokenType
-from cogelot.structures.vima import PoseActionType
+from cogelot.structures.vima import (
+    ActionBounds,
+    PoseActionType,
+)
 from vima import nn as vnn
 from vima.nn.action_decoder.dists import MultiCategorical
 from vima.utils import DataDict
@@ -23,6 +26,150 @@ def get_max_length_of_prompt(raw_prompts_token_type: RawPromptTokenType, max_num
     )
     max_length = max(length_per_prompt)
     return max_length
+
+
+def get_max_num_objects_from_embedded_observations(embedded_observations: torch.Tensor) -> int:
+    """Get the maximum number of objects from the embedded observations."""
+    return embedded_observations.shape[-2]
+
+
+def discretize_action(
+    action: dict[PoseActionType, torch.Tensor],
+    *,
+    n_discrete_x_bins: int,
+    n_discrete_y_bins: int,
+    n_discrete_rot_bins: int,
+) -> dict[PoseActionType, torch.Tensor]:
+    """Discretize the action."""
+    device = action["pose0_position"].device
+    boundary_x = torch.linspace(start=0, end=1, steps=n_discrete_x_bins, device=device)
+    boundary_y = torch.linspace(start=0, end=1, steps=n_discrete_y_bins, device=device)
+    boundary_rot = torch.linspace(start=0, end=1, steps=n_discrete_rot_bins, device=device)
+
+    action["pose0_position"][..., 0] = torch.bucketize(
+        action["pose0_position"][..., 0].contiguous(), boundary_x
+    )
+    action["pose0_position"][..., 1] = torch.bucketize(
+        action["pose0_position"][..., 1].contiguous(), boundary_y
+    )
+    action["pose0_rotation"] = torch.bucketize(action["pose0_rotation"].contiguous(), boundary_rot)
+
+    action["pose1_position"][..., 0] = torch.bucketize(
+        action["pose1_position"][..., 0].contiguous(), boundary_x
+    )
+    action["pose1_position"][..., 1] = torch.bucketize(
+        action["pose1_position"][..., 1].contiguous(), boundary_y
+    )
+    action["pose1_rotation"] = torch.bucketize(action["pose1_rotation"].contiguous(), boundary_rot)
+    action = {k: v.long() for k, v in action.items()}
+    return action
+
+
+def de_discretize_actions(
+    actions: dict[PoseActionType, torch.Tensor],
+    *,
+    n_discrete_x_bins: int,
+    n_discrete_y_bins: int,
+    n_discrete_rot_bins: int,
+) -> dict[PoseActionType, torch.Tensor]:
+    """De-discretize the actions."""
+    actions = {k: v.float() for k, v in actions.items()}
+    actions["pose0_position"][..., 0] = actions["pose0_position"][..., 0] / n_discrete_x_bins
+    actions["pose0_position"][..., 1] = actions["pose0_position"][..., 1] / n_discrete_y_bins
+    actions["pose0_rotation"] = actions["pose0_rotation"] / n_discrete_rot_bins
+
+    actions["pose1_position"][..., 0] = actions["pose1_position"][..., 0] / n_discrete_x_bins
+    actions["pose1_position"][..., 1] = actions["pose1_position"][..., 1] / n_discrete_y_bins
+    actions["pose1_rotation"] = actions["pose1_rotation"] / n_discrete_rot_bins
+    return actions
+
+
+def clamp_actions_to_bounds(
+    actions: dict[PoseActionType, torch.Tensor], *, action_bounds: ActionBounds
+) -> dict[PoseActionType, torch.Tensor]:
+    """Clamp the actions to the bounds of the action space."""
+    device = actions["pose0_position"].device
+
+    # Clamp the continuous actions to the action bounds
+    action_bounds_low = torch.from_numpy(action_bounds.low).to(device)
+    action_bounds_high = torch.from_numpy(action_bounds.high).to(device)
+
+    actions["pose0_position"] = (
+        actions["pose0_position"] * (action_bounds_high - action_bounds_low) + action_bounds_low
+    )
+    actions["pose1_position"] = (
+        actions["pose1_position"] * (action_bounds_high - action_bounds_low) + action_bounds_low
+    )
+    actions["pose0_position"] = torch.clamp(
+        actions["pose0_position"], min=action_bounds_low, max=action_bounds_high
+    )
+    actions["pose1_position"] = torch.clamp(
+        actions["pose1_position"], min=action_bounds_low, max=action_bounds_high
+    )
+    actions["pose0_rotation"] = actions["pose0_rotation"] * 2 - 1
+    actions["pose1_rotation"] = actions["pose1_rotation"] * 2 - 1
+    actions["pose0_rotation"] = torch.clamp(actions["pose0_rotation"], min=-1, max=1)
+    actions["pose1_rotation"] = torch.clamp(actions["pose1_rotation"], min=-1, max=1)
+
+    return actions
+
+
+def stitch_observations_with_actions(  # noqa: WPS210
+    embedded_observations: torch.Tensor,
+    embedded_observations_mask: torch.Tensor,
+    embedded_actions: torch.Tensor | None,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Stitch the observations together with actions for decoder input."""
+    embed_dim = embedded_observations.size(-1)
+    batch_size, observation_seq_len = embedded_observations.shape[:2]
+    actions_seq_len = 0 if embedded_actions is None else embedded_actions.shape[1]
+
+    if observation_seq_len not in {actions_seq_len, actions_seq_len + 1}:
+        raise AssertionError(
+            "The number of observations must be equal to or one more than the number of actions"
+        )
+
+    max_objects = get_max_num_objects_from_embedded_observations(embedded_observations)
+    total_seq_len = observation_seq_len * max_objects + actions_seq_len
+
+    # Rearrange the tensors to be in the right structure
+    # embedded_observations = rearrange(embedded_observations, "L B Q E -> B L Q E")
+    embedded_observations = rearrange(embedded_observations, "B L Q E -> B (L Q) E")
+    embedded_observations = rearrange(embedded_observations, "B L E -> L B E")
+
+    # embedded_observations_mask = rearrange(embedded_observations_mask, "L B Q -> B L Q")
+    embedded_observations_mask = rearrange(embedded_observations_mask, "B L Q -> B (L Q)")
+    embedded_observations_mask = rearrange(embedded_observations_mask, "B L -> L B")
+
+    # Create tensors which will we will use to put the various tokens into
+    tokens = torch.empty(
+        total_seq_len,
+        batch_size,
+        embed_dim,
+        dtype=torch.float32,
+        device=embedded_observations.device,
+    )
+    masks = torch.ones(
+        total_seq_len, batch_size, dtype=torch.bool, device=embedded_observations.device
+    )
+
+    # Fill in the tokens and masks properly
+    for obj_idx in range(max_objects):
+        tokens[obj_idx :: max_objects + 1] = embedded_observations[  # noqa: WPS362
+            obj_idx::max_objects
+        ]
+        masks[obj_idx :: max_objects + 1] = embedded_observations_mask[  # noqa: WPS362
+            obj_idx::max_objects
+        ]
+
+    if embedded_actions is not None:
+        tokens[max_objects :: max_objects + 1] = embedded_actions.transpose(0, 1)  # noqa: WPS362
+
+    # Put the batch first
+    tokens = rearrange(tokens, "L B E -> B L E")
+    masks = rearrange(masks, "L B -> B L")
+
+    return tokens, masks
 
 
 class Policy(torch.nn.Module):
@@ -75,8 +222,8 @@ class Policy(torch.nn.Module):
         embedded_actions: torch.Tensor | None,
     ) -> torch.Tensor:
         """Predict the action token."""
-        max_objects = self._get_max_num_objects(embedded_observations=embedded_observations)
-        tokens, masks = self._stitch_observations_with_actions(
+        max_objects = get_max_num_objects_from_embedded_observations(embedded_observations)
+        tokens, masks = stitch_observations_with_actions(
             embedded_observations, embedded_observations_mask, embedded_actions
         )
 
@@ -220,9 +367,15 @@ class Policy(torch.nn.Module):
         self, actions: DataDict | dict[PoseActionType, torch.Tensor]
     ) -> torch.Tensor:
         """Embed the actions into a tensor."""
-        return self._action_encoder(
-            self.de_discretize_actions(cast(dict[PoseActionType, torch.Tensor], actions))
+        embedded_actions = self._action_encoder(
+            de_discretize_actions(
+                cast(dict[PoseActionType, torch.Tensor], actions),
+                n_discrete_x_bins=self.n_discrete_x_bins,
+                n_discrete_y_bins=self.n_discrete_y_bins,
+                n_discrete_rot_bins=self.n_discrete_rot_bins,
+            )
         )
+        return embedded_actions
 
     def encode_prompt(
         self, embedded_prompt: torch.Tensor, embedded_prompt_mask: torch.Tensor
@@ -239,121 +392,3 @@ class Policy(torch.nn.Module):
     ) -> dict[PoseActionType, MultiCategorical]:
         """Decode the action token."""
         return self._action_decoder(predicted_action_tokens)
-
-    def discretize_action(
-        self, action: dict[PoseActionType, torch.Tensor]
-    ) -> dict[PoseActionType, torch.Tensor]:
-        """Discretize the action."""
-        device = action["pose0_position"].device
-        boundary_x = torch.linspace(start=0, end=1, steps=self.n_discrete_x_bins, device=device)
-        boundary_y = torch.linspace(start=0, end=1, steps=self.n_discrete_y_bins, device=device)
-        boundary_rot = torch.linspace(
-            start=0, end=1, steps=self.n_discrete_rot_bins, device=device
-        )
-
-        action["pose0_position"][..., 0] = torch.bucketize(
-            action["pose0_position"][..., 0].contiguous(), boundary_x
-        )
-        action["pose0_position"][..., 1] = torch.bucketize(
-            action["pose0_position"][..., 1].contiguous(), boundary_y
-        )
-        action["pose0_rotation"] = torch.bucketize(
-            action["pose0_rotation"].contiguous(), boundary_rot
-        )
-
-        action["pose1_position"][..., 0] = torch.bucketize(
-            action["pose1_position"][..., 0].contiguous(), boundary_x
-        )
-        action["pose1_position"][..., 1] = torch.bucketize(
-            action["pose1_position"][..., 1].contiguous(), boundary_y
-        )
-        action["pose1_rotation"] = torch.bucketize(
-            action["pose1_rotation"].contiguous(), boundary_rot
-        )
-        action = {k: v.long() for k, v in action.items()}
-        return action
-
-    def de_discretize_actions(
-        self, actions: dict[PoseActionType, torch.Tensor]
-    ) -> dict[PoseActionType, torch.Tensor]:
-        """De-discretize the actions."""
-        actions = {k: v.float() for k, v in actions.items()}
-        actions["pose0_position"][..., 0] = (
-            actions["pose0_position"][..., 0] / self.n_discrete_x_bins
-        )
-        actions["pose0_position"][..., 1] = (
-            actions["pose0_position"][..., 1] / self.n_discrete_y_bins
-        )
-        actions["pose0_rotation"] = actions["pose0_rotation"] / self.n_discrete_rot_bins
-
-        actions["pose1_position"][..., 0] = (
-            actions["pose1_position"][..., 0] / self.n_discrete_x_bins
-        )
-        actions["pose1_position"][..., 1] = (
-            actions["pose1_position"][..., 1] / self.n_discrete_y_bins
-        )
-        actions["pose1_rotation"] = actions["pose1_rotation"] / self.n_discrete_rot_bins
-        return actions
-
-    def _stitch_observations_with_actions(
-        self,
-        embedded_observations: torch.Tensor,
-        embedded_observations_mask: torch.Tensor,
-        embedded_actions: torch.Tensor | None,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        """Stitch the observations together with actions for decoder input."""
-        batch_size, observation_seq_len = embedded_observations.shape[:2]
-        actions_seq_len = 0 if embedded_actions is None else embedded_actions.shape[1]
-
-        if observation_seq_len != actions_seq_len + 1:
-            raise AssertionError(
-                "The number of observations must be one more than the number of actions"
-            )
-
-        max_objects = self._get_max_num_objects(embedded_observations=embedded_observations)
-        total_seq_len = observation_seq_len * max_objects + actions_seq_len
-
-        # Rearrange the tensors to be in the right structure
-        # embedded_observations = rearrange(embedded_observations, "L B Q E -> B L Q E")
-        embedded_observations = rearrange(embedded_observations, "B L Q E -> B (L Q) E")
-        embedded_observations = rearrange(embedded_observations, "B L E -> L B E")
-
-        # embedded_observations_mask = rearrange(embedded_observations_mask, "L B Q -> B L Q")
-        embedded_observations_mask = rearrange(embedded_observations_mask, "B L Q -> B (L Q)")
-        embedded_observations_mask = rearrange(embedded_observations_mask, "B L -> L B")
-
-        # Create tensors which will we will use to put the various tokens into
-        tokens = torch.empty(
-            total_seq_len,
-            batch_size,
-            self.embed_dim,
-            dtype=torch.float32,
-            device=embedded_observations.device,
-        )
-        masks = torch.ones(
-            total_seq_len, batch_size, dtype=torch.bool, device=embedded_observations.device
-        )
-
-        # Fill in the tokens and masks properly
-        for obj_idx in range(max_objects):
-            tokens[obj_idx :: max_objects + 1] = embedded_observations[  # noqa: WPS362
-                obj_idx::max_objects
-            ]
-            masks[obj_idx :: max_objects + 1] = embedded_observations_mask[  # noqa: WPS362
-                obj_idx::max_objects
-            ]
-
-        if embedded_actions is not None:
-            tokens[max_objects :: max_objects + 1] = embedded_actions.transpose(  # noqa: WPS362
-                0, 1
-            )
-
-        # Put the batch first
-        tokens = rearrange(tokens, "L B E -> B L E")
-        masks = rearrange(masks, "L B -> B L")
-
-        return tokens, masks
-
-    def _get_max_num_objects(self, *, embedded_observations: torch.Tensor) -> int:
-        """Get the max number of objects."""
-        return embedded_observations.shape[-2]
