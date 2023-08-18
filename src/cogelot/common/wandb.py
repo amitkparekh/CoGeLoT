6e1c803e7b530059c6043df565fbb33ed5625f23
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-18 15:08:55 +0100

    feat: only download the model if it does not already exist

--- src/cogelot/common/wandb.py
+++ src/cogelot/common/wandb.py
@@ -3,15 +3,23 @@ from pathlib import Path
 import wandb
 
 
-def download_model_from_wandb(entity: str, project: str, run_id: str, save_dir: Path) -> Path:
+def download_model_from_wandb(
+    entity: str, project: str, run_id: str, save_dir: Path, *, force_download: bool = False
+) -> Path:
     """Downlaod the model from wandb into the output dir and return the path to it."""
+    checkpoint_name = f"{entity}_{project}_{run_id}.ckpt"
+    checkpoint_path = save_dir.joinpath(checkpoint_name)
+
+    if checkpoint_path.exists() and not force_download:
+        return checkpoint_path
+
     api = wandb.Api()
     run = api.run(f"{entity}/{project}/{run_id}")
 
     run_artifacts = run.logged_artifacts()
-    assert len(run_artifacts) == 1, "There should only be one artifact in the run."
+    model_artifacts = [artifact for artifact in run_artifacts if artifact.type == "model"]
+    assert len(model_artifacts) == 1, "There should only be one artifact in the run."
 
-    model_artifact = run_artifacts[0]
-    checkpoint_path = model_artifact.download(str(save_dir))
+    checkpoint_path = model_artifacts[0].download(str(checkpoint_path))
 
     return Path(checkpoint_path)
