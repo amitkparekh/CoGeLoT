91c5bc35590ed778e688d5e92d42308e42396462
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-18 16:09:38 +0100

    feat: create a command to download models from wandb since they can take a while

---
+++ src/cogelot/commands/download_model.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import typer
+from loguru import logger
+
+from cogelot.common.wandb import download_model_from_wandb
+
+
+app = typer.Typer()
+
+
+@app.command()
+def download_model(
+    run_id: str,
+    *,
+    entity: str = "pyop",
+    project: str = "CoGeLoT",
+    save_dir: Path = Path("./storage/data/models"),
+    force_download: bool = False,
+) -> None:
+    """Download the model checkpoint from wandb."""
+    logger.info("Downloading model from wandb...")
+
+    checkpoint_path = download_model_from_wandb(
+        entity, project, run_id, save_dir, force_download=force_download
+    )
+
+    logger.info(f"Model downloaded to {checkpoint_path}")
+
+
+if __name__ == "__main__":
+    app()
