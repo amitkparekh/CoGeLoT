0ce8e1176efdefa689ddd6fc753d2ffb7e02be1c
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-18 16:09:20 +0100

    feat: improve the function for download the model from wandb

--- src/cogelot/common/wandb.py
+++ src/cogelot/common/wandb.py
@@ -10,16 +10,23 @@ def download_model_from_wandb(
     checkpoint_name = f"{entity}_{project}_{run_id}.ckpt"
     checkpoint_path = save_dir.joinpath(checkpoint_name)
 
+    # If the checkpoint already exists, don't download it again
     if checkpoint_path.exists() and not force_download:
         return checkpoint_path
 
+    # Get the run from wandb
     api = wandb.Api()
     run = api.run(f"{entity}/{project}/{run_id}")
 
-    run_artifacts = run.logged_artifacts()
-    model_artifacts = [artifact for artifact in run_artifacts if artifact.type == "model"]
+    # Get the model artifact from the run
+    model_artifacts = [artifact for artifact in run.logged_artifacts() if artifact.type == "model"]
     assert len(model_artifacts) == 1, "There should only be one artifact in the run."
 
-    checkpoint_path = model_artifacts[0].download(str(checkpoint_path))
+    # Download the model artifact (this can take a while)
+    model_artifact_dir: str = model_artifacts[0].download(str(save_dir))
 
-    return Path(checkpoint_path)
+    # Rename the file to the expected name/location
+    model_checkpoint_file = next(Path(model_artifact_dir).rglob("*.ckpt"))
+    checkpoint_path = model_checkpoint_file.rename(checkpoint_path)
+
+    return checkpoint_path
