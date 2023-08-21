b950ee4085dab962eff27317e179defc50b8359a
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-21 13:20:03 +0100

    test: fix vima instance fixture names

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -3,28 +3,28 @@ from cogelot.structures.vima import VIMAInstance
 
 
 def test_text_tokenizer_does_not_fail(
-    normalized_instance: VIMAInstance, text_tokenizer: TextTokenizer
+    vima_instance: VIMAInstance, text_tokenizer: TextTokenizer
 ) -> None:
-    tokenized_output = text_tokenizer.tokenizer(normalized_instance.prompt)
+    tokenized_output = text_tokenizer.tokenizer(vima_instance.prompt)
     assert tokenized_output is not None
 
 
 def test_image_tokenizer_does_not_fail(
-    normalized_instance: VIMAInstance, image_tokenizer: ImageTokenizer
+    vima_instance: VIMAInstance, image_tokenizer: ImageTokenizer
 ) -> None:
     tokens = [
         image_tokenizer.tokenize_observation(
-            observation=observation, all_object_ids=normalized_instance.object_ids
+            observation=observation, all_object_ids=vima_instance.object_ids
         )
-        for observation in normalized_instance.observations
+        for observation in vima_instance.observations
     ]
     assert tokens is not None
 
 
 def test_pose_action_tokenizer_works_properly(
-    normalized_instance: VIMAInstance, pose_action_tokenizer: PoseActionTokenizer
+    vima_instance: VIMAInstance, pose_action_tokenizer: PoseActionTokenizer
 ) -> None:
-    tokenized_actions = pose_action_tokenizer.tokenize(normalized_instance.pose_actions)
+    tokenized_actions = pose_action_tokenizer.tokenize(vima_instance.pose_actions)
 
     assert tokenized_actions
 
