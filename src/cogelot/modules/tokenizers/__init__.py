bbc98a67cfd9660dcce98371ad4bb4f8f2598019
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-22 14:50:07 +0100

    refactor: delete modules since they are never used
    they are from an old "possible refactor" which would be useful in moving away from the `DataDict`'s, but I'm stuck with them

--- src/cogelot/modules/tokenizers/__init__.py
+++ src/cogelot/modules/tokenizers/__init__.py
@@ -1,7 +1,5 @@
 from cogelot.modules.tokenizers.end_effector import EndEffectorTokenizer
 from cogelot.modules.tokenizers.image import ImageTokenizer
-from cogelot.modules.tokenizers.multimodal_history import MultimodalHistoryTokenizer
-from cogelot.modules.tokenizers.multimodal_prompt import MultimodalPromptTokenizer
 from cogelot.modules.tokenizers.observation import ObservationTokenizer
 from cogelot.modules.tokenizers.pose_action import PoseActionTokenizer
 from cogelot.modules.tokenizers.text import (
