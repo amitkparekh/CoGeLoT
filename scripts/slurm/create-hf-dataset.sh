444117051344c7ada88b43c6bcc5e9885a53f329
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-28 11:11:48 +0100

    fix(script): update the create hf dataset slurm script

--- scripts/slurm/create-hf-dataset.sh
+++ scripts/slurm/create-hf-dataset.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
-#SBATCH --mem=200G
+#SBATCH --mem=400G
 #SBATCH --time=24:00:00
 #SBATCH --partition=nodes
 #SBATCH --job-name=create-dataset
@@ -9,5 +9,4 @@
 #SBATCH --output=jobs/create-dataset.%J.out
 #SBATCH -p nodes
 
-pdm run python -m cogelot parse-raw-dataset --num-workers 30
-pdm run python -m cogelot create-hf-dataset --num-workers 30
+pdm run python -m cogelot create_raw_dataset --num-workers 30
