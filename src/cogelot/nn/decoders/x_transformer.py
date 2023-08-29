dcfe25aa31d39999750be998e4d5ccf9b8d13441
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-29 09:42:28 +0100

    feat: support a separate embedder for the memory input

--- src/cogelot/nn/decoders/x_transformer.py
+++ src/cogelot/nn/decoders/x_transformer.py
@@ -1,7 +1,10 @@
+from typing import Any
+
 import torch
 from x_transformers import ContinuousTransformerWrapper
 
 from cogelot.nn.decoders.interfaces import TransformerDecoderProtocol
+from cogelot.nn.embedder import EmbedderProtocol
 
 
 def _create_padding_mask_from_tensor(tensor: torch.Tensor) -> torch.Tensor:
@@ -13,12 +16,32 @@ def _create_padding_mask_from_tensor(tensor: torch.Tensor) -> torch.Tensor:
     )
 
 
-class PromptEncoderHistoryDecoder(TransformerDecoderProtocol, ContinuousTransformerWrapper):  # type: ignore[misc]
-    """XTransformers wrapper for the encoder-decoder, with the prompt in the encoder.
+class TransformerDecoder(TransformerDecoderProtocol, ContinuousTransformerWrapper):  # type: ignore[misc]
+    """XTransformers wrapper for the decoder.
 
     This is a wrapper around the `ContinuousTransformerWrapper` from the `x-transformers` library,
     so that the signature of the forward is the same.
     """
+
+    def __init__(self, xattn_embedder: EmbedderProtocol, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.xattn_embedder = xattn_embedder
+
+    def _add_position_embedding_to_memory(
+        self, memory: torch.Tensor, memory_key_padding_mask: torch.Tensor
+    ) -> torch.Tensor:
+        """Add the position embedding to the memory, a.k.a.
+
+        the cross-attention input.
+        """
+        memory_position_ids = (torch.cumsum(memory_key_padding_mask, dim=1) - 1).long()
+        embedded_memory_position = self.xattn_embedder.forward(memory, memory_position_ids)
+        memory_with_position = memory + embedded_memory_position
+        return memory_with_position
+
+
+class PromptEncoderHistoryDecoder(TransformerDecoder):
+    """XTransformers wrapper for the encoder-decoder, with the prompt in the encoder."""
 
     def forward(
         self,
@@ -38,6 +61,8 @@ class PromptEncoderHistoryDecoder(TransformerDecoderProtocol, ContinuousTransfor
         # Create the position ids from the mask (just how they do in the Policy)
         position_ids = (torch.cumsum(tgt_key_padding_mask, dim=1) - 1).long()
 
+        memory = self._add_position_embedding_to_memory(memory, memory_key_padding_mask)
+
         model_output = ContinuousTransformerWrapper.forward(
             self,
             x=tgt,
@@ -50,15 +75,27 @@ class PromptEncoderHistoryDecoder(TransformerDecoderProtocol, ContinuousTransfor
         return model_output
 
 
-class ConventionalEncoderDecoder(TransformerDecoderProtocol, ContinuousTransformerWrapper):
-    """XTransformers wrapper for an encoder-decoder model, with the prompt in the encoder.
+class ConventionalEncoderDecoder(TransformerDecoder):
+    """XTransformers wrapper for an encoder-decoder model, but 'normally'.
 
     This is the more conventional way of doing it: by putting the prompt and all the history as
     input from cross-attention, and just decoding the outputs.
     """
 
+    def forward(
+        self,
+        tgt: torch.Tensor,
+        memory: torch.Tensor,
+        tgt_mask: torch.Tensor | None = None,
+        memory_mask: torch.Tensor | None = None,
+        tgt_key_padding_mask: torch.Tensor | None = None,
+        memory_key_padding_mask: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        """Forward pass of the decoder."""
+        raise NotImplementedError
 
-class DecoderOnly(TransformerDecoderProtocol, ContinuousTransformerWrapper):  # type: ignore[misc]
+
+class DecoderOnly(TransformerDecoder):
     """XTransformers wrapper for the decoder-only model.
 
     For any given input, the memory is prepended to the target sequence so that it all goes in the
@@ -82,6 +119,9 @@ class DecoderOnly(TransformerDecoderProtocol, ContinuousTransformerWrapper):  # 
             tgt_key_padding_mask = _create_padding_mask_from_tensor(tgt)
         if memory_key_padding_mask is None:
             memory_key_padding_mask = _create_padding_mask_from_tensor(memory)
+
+        # Add separate position embeddings to the memory
+        memory = self._add_position_embedding_to_memory(memory, memory_key_padding_mask)
 
         # Prepend the memory to the target sequence
         tgt = torch.cat([memory, tgt], dim=1)
---
+++ src/cogelot/nn/embedder.py
@@ -0,0 +1,21 @@
+import abc
+
+import torch
+
+
+class EmbedderProtocol(abc.ABC, torch.nn.Module):
+    """Protocol for the Embedder from x-transformers."""
+
+    def forward(self, x: torch.Tensor, pos: torch.Tensor | None = None) -> torch.Tensor:
+        """Forward pass of the embedder, returning embedded instances."""
+        ...  # noqa: WPS428
+
+
+class PassthroughEmbedder(EmbedderProtocol):
+    """Embedder that just returns the input and does nothing."""
+
+    def forward(
+        self, x: torch.Tensor, pos: torch.Tensor | None = None  # noqa: ARG002
+    ) -> torch.Tensor:
+        """Just return the input."""
+        return torch.zeros_like(x)
