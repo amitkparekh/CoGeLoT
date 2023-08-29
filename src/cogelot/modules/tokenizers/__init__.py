72db2ba9701773991349f6618544ad2cfddf7853
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-08-29 09:36:04 +0100

    refactor: remove unused tokenizers

--- src/cogelot/modules/tokenizers/__init__.py
+++ src/cogelot/modules/tokenizers/__init__.py
@@ -1,6 +1,4 @@
 from cogelot.modules.tokenizers.end_effector import EndEffectorTokenizer
-from cogelot.modules.tokenizers.image import ImageTokenizer
-from cogelot.modules.tokenizers.observation import ObservationTokenizer
 from cogelot.modules.tokenizers.pose_action import PoseActionTokenizer
 from cogelot.modules.tokenizers.text import (
     PLACEHOLDER_TOKENS,
