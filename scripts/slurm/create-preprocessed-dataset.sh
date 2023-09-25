5f46509ac9550c0ff7823424d48d2dfa1513d688
Amit Parekh <ap41@node02.pri.dmog.alces.network>
2023-09-25 16:16:15 +0100

    chore(slurm): add create preprocessed dataset script across multiple jobs
    Committer: Amit Parekh <7276308+amitkparekh@users.noreply.github.com>

---
+++ scripts/slurm/create-preprocessed-dataset.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=75G
+#SBATCH --time=12:00:00
+#SBATCH --partition=nodes
+#SBATCH --job-name=create-preprocessed-dataset
+#SBATCH --error=jobs/%x-%a.%J.err
+#SBATCH --output=jobs/%x-%a.%J.out
+#SBATCH --array=0-18
+
+
+export DATASETS_VERBOSITY=info
+
+pdm run python -m cogelot create-preprocessed-dataset-per-task --num-workers 15 --task-index-filter ${SLURM_ARRAY_TASK_ID}
