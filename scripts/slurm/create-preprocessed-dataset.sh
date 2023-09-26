b23bd8d0d61c26a4d5372040a44dca00c5c9a280
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-09-26 15:07:49 +0100

    fix: trying again to fix the output file name for slurm jobs

--- scripts/slurm/create-preprocessed-dataset.sh
+++ scripts/slurm/create-preprocessed-dataset.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --partition=nodes
 #SBATCH --job-name=create-preprocessed-dataset
-#SBATCH --output=jobs/%x.%J-%a.out
+#SBATCH --output=jobs/%x.%A-%a.out
 #SBATCH --array=0-17
 
 export DATASETS_VERBOSITY=info
--- scripts/slurm/preprocess-instances.sh
+++ scripts/slurm/preprocess-instances.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --partition=nodes
 #SBATCH --job-name=preprocess-instances
-#SBATCH --output=jobs/%x.%J-%a.out
+#SBATCH --output=jobs/%x.%A-%a.out
 #SBATCH --array=0-17
 
 pdm run python -m cogelot preprocess-instances --num-workers 40 --task-index-filter "${SLURM_ARRAY_TASK_ID}"
