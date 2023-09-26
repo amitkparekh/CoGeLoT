b4b6345e22c00bc070eb949ede0ed38d2481f9a6
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-09-26 20:49:10 +0100

    feat: refactor and expand the metrics to include per task metrics for each individual axis
    Yes this is a lot of metrics, but I'm not half-arsing this

--- src/cogelot/models/training.py
+++ src/cogelot/models/training.py
@@ -6,11 +6,12 @@ from typing import Any, Literal, Self
 
 import pytorch_lightning as pl
 import torch
-from torchmetrics import SumMetric
 
 from cogelot.common.checkpoint import create_hparams_for_checkpoint
 from cogelot.common.wandb import download_model_from_wandb
-from cogelot.modules.metrics import LossPerAxisPerActionMetric, PoseAccuracyPerAxisMetric
+from cogelot.modules.metrics import (
+    TrainingMetrics,
+)
 from cogelot.modules.policy import Policy
 from cogelot.nn.loss import compute_fine_grained_loss, reduce_fine_grained_loss
 from cogelot.structures.model import ModelInstance, PreprocessedBatch
@@ -50,14 +51,12 @@ class VIMALightningModule(pl.LightningModule):
         self._optimizer_partial_fn = optimizer_partial_fn
         self._lr_scheduler_partial_fn = lr_scheduler_partial_fn
 
-        self._accuracy = PoseAccuracyPerAxisMetric(
+        self.metrics = TrainingMetrics(
             max_num_classes=max(
                 N_DISCRETE_X_BINS, N_DISCRETE_Y_BINS, N_DISCRETE_Z_BINS, N_DISCRETE_ROT_BINS
             ),
             ignore_index=self.ignore_target_index,
         )
-        self._loss_per_axis = LossPerAxisPerActionMetric()
-        self._examples_seen = SumMetric(compute_on_cpu=True)
         self.save_hyperparameters(logger=False)
 
     @classmethod
@@ -111,17 +110,14 @@ class VIMALightningModule(pl.LightningModule):
         )
         loss = reduce_fine_grained_loss(fine_grained_loss)
 
-        self._loss_per_axis.update(fine_grained_loss)
-        self._update_accuracy(predicted_actions, target_actions)
-
-        self.log("train_loss", loss, prog_bar=True, logger=True, batch_size=len(batch))
-        self._log_accuracy(split="train", prog_bar=True, logger=True, batch_size=len(batch))
-        self._log_loss_per_axis(split="train", prog_bar=True, logger=True, batch_size=len(batch))
-
+        self.metrics.update_loss(fine_grained_loss, tasks=batch.task)
+        self.metrics.update_accuracy(predicted_actions, target_actions, tasks=batch.task)
         # Log the total number of examples seen across all epochs (and doing it this way will
         # prevent the thing resetting every epoch)
-        self._examples_seen.update(len(batch))
-        self.log("trainer/num_examples", self._examples_seen.compute(), prog_bar=True, logger=True)
+        self.metrics.update_examples_seen(len(batch))
+
+        self.log("train_loss", loss, prog_bar=True, logger=True, batch_size=len(batch))
+        self._log_metrics(split="val", prog_bar=True, logger=True, batch_size=len(batch))
 
         return loss
 
@@ -138,16 +134,15 @@ class VIMALightningModule(pl.LightningModule):
         )
         loss = reduce_fine_grained_loss(fine_grained_loss)
 
-        self._loss_per_axis.update(fine_grained_loss)
-        self._update_accuracy(predicted_actions, target_actions)
+        self.metrics.update_loss(fine_grained_loss, tasks=batch.task)
+        self.metrics.update_accuracy(predicted_actions, target_actions, tasks=batch.task)
 
         # There may be a PossibleUserWarning about needing to use `sync_dist=True` here. However,
         # as we are using torchmetrics, we should not/do not need to be adding that flag. For more,
         # see: https://github.com/Lightning-AI/lightning/discussions/6501#discussioncomment-553152
         # and https://lightning.ai/docs/pytorch/stable/accelerators/accelerator_prepare.html#synchronize-validation-and-test-logging
         self.log("val_loss", loss, prog_bar=True, logger=True, batch_size=len(batch))
-        self._log_accuracy(split="val", prog_bar=True, logger=True, batch_size=len(batch))
-        self._log_loss_per_axis(split="val", prog_bar=True, logger=True, batch_size=len(batch))
+        self._log_metrics(split="val", prog_bar=True, logger=True, batch_size=len(batch))
 
         return loss
 
@@ -167,14 +162,12 @@ class VIMALightningModule(pl.LightningModule):
 
     def on_train_batch_end(self, *args: Any, **kwargs: Any) -> None:
         """Reset the accuracy metric at the end of the epoch."""
-        self._accuracy.reset()
-        self._loss_per_axis.reset()
+        self.metrics.reset()
         return super().on_train_batch_end(*args, **kwargs)
 
     def on_validation_epoch_end(self) -> None:
         """Reset the accuracy metric at the end of the epoch."""
-        self._accuracy.reset()
-        self._loss_per_axis.reset()
+        self.metrics.reset()
         return super().on_validation_epoch_end()
 
     def embed_inputs(self, batch: PreprocessedBatch) -> ModelInstance:
@@ -197,33 +190,6 @@ class VIMALightningModule(pl.LightningModule):
         )
 
     @torch.no_grad()
-    def _update_accuracy(
-        self,
-        predicted_actions: dict[PoseActionType, MultiCategorical],
-        target_actions: dict[PoseActionType, torch.Tensor],
-    ) -> None:
-        """Update the accuracy metric for all the pose action types."""
-        predicted_action_tokens: dict[PoseActionType, torch.Tensor] = {
-            pose_action_type: action_distribution.mode()
-            for pose_action_type, action_distribution in predicted_actions.items()
-        }
-        self._accuracy.update(predicted_action_tokens, target_actions)
-
-    @torch.no_grad()
-    def _log_accuracy(self, *, split: Literal["train", "val"], **log_dict_kwargs: Any) -> None:
+    def _log_metrics(self, *, split: Literal["train", "val"], **log_dict_kwargs: Any) -> None:
         """Log the accuracy for the given split."""
-        computed_acc = {
-            f"{split}_{pose_name}_acc": acc for pose_name, acc in self._accuracy.compute().items()
-        }
-        self.log_dict(computed_acc, **log_dict_kwargs)
-
-    @torch.no_grad()
-    def _log_loss_per_axis(
-        self, *, split: Literal["train", "val"], **log_dict_kwargs: Any
-    ) -> None:
-        """Log the loss per axis."""
-        computed_loss = {
-            f"{split}_{loss_name}_loss": loss
-            for loss_name, loss in self._loss_per_axis.compute().items()
-        }
-        self.log_dict(computed_loss, **log_dict_kwargs)
+        self.log_dict(self.metrics.compute(split=split), **log_dict_kwargs)
--- src/cogelot/modules/metrics.py
+++ src/cogelot/modules/metrics.py
@@ -1,4 +1,4 @@
-from typing import ClassVar, get_args
+from typing import TYPE_CHECKING, ClassVar, Literal, get_args
 
 import torch
 import wandb
@@ -15,6 +15,11 @@ from cogelot.structures.vima import (
     Task,
     get_task_group_from_task,
 )
+from vima.nn.action_decoder.dists import MultiCategorical
+
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
 
 
 def _create_metric_key_per_axis(string_template: str = PER_AXIS_KEY_TEMPLATE) -> list[str]:
@@ -26,12 +31,17 @@ def _create_metric_key_per_axis(string_template: str = PER_AXIS_KEY_TEMPLATE) ->
     ]
 
 
+def _include_task_in_metric_key(metric_key: str, task: Task) -> str:
+    """Include the task in the metric key."""
+    return f"{task.value + 1:02d}_{metric_key}"
+
+
 class PoseAccuracyPerAxisMetric(MultitaskWrapper):
     """Accuracy metric for the pose action."""
 
     def __init__(self, max_num_classes: int, ignore_index: int) -> None:
         metric_keys = _create_metric_key_per_axis()
-        metrics = {
+        metrics: Mapping[str, MulticlassAccuracy] = {
             key: MulticlassAccuracy(num_classes=max_num_classes, ignore_index=ignore_index)
             for key in metric_keys
         }
@@ -53,6 +63,66 @@ class PoseAccuracyPerAxisMetric(MultitaskWrapper):
         targets_per_axis = PerActionPerAxis.from_actions(targets).to_flattened_dict()
 
         MultitaskWrapper.update(self, predictions_per_axis, targets_per_axis)
+
+
+class PoseAccuracyPerAxisPerTaskMetric(MultitaskWrapper):
+    """Accuracy metric for each axis for each pose, separated by task."""
+
+    def __init__(self, max_num_classes: int, ignore_index: int) -> None:
+        metric_keys = [
+            _include_task_in_metric_key(key, task)
+            for key in _create_metric_key_per_axis()
+            for task in Task
+        ]
+        metrics = {
+            key: MulticlassAccuracy(num_classes=max_num_classes, ignore_index=ignore_index)
+            for key in metric_keys
+        }
+        super().__init__(metrics)  # pyright: ignore[reportGeneralTypeIssues]
+
+    def update(
+        self,
+        predictions: dict[PoseActionType, torch.Tensor],
+        targets: dict[PoseActionType, torch.Tensor],
+        *,
+        tasks: list[Task],
+    ) -> None:
+        """Update the metrics for each axis."""
+        if predictions.keys() != targets.keys():
+            raise ValueError(
+                f"Keys for the predictions ({predictions.keys()}) for not match the keys for the"
+                f" targets ({targets.keys()})"
+            )
+
+        predictions_per_axis = PerActionPerAxis.from_actions(predictions).to_flattened_dict()
+        targets_per_axis = PerActionPerAxis.from_actions(targets).to_flattened_dict()
+
+        # Split each tensor within each dictionary into a list of tensors, one for each task. We
+        # are splitting across the batch dimension.
+        predictions_per_task_per_axis = {
+            task: {key: tensor[idx] for key, tensor in predictions_per_axis.items()}
+            for idx, task in enumerate(tasks)
+        }
+        targets_per_task_per_axis = {
+            task: {key: tensor[idx] for key, tensor in targets_per_axis.items()}
+            for idx, task in enumerate(tasks)
+        }
+
+        # Reshape the dict into a single dict, with the task as part of the key
+        flattened_predictions_per_task_per_axis = {
+            _include_task_in_metric_key(key, task): tensor
+            for task, predictions_per_axis in predictions_per_task_per_axis.items()
+            for key, tensor in predictions_per_axis.items()
+        }
+        flattened_targets_per_task_per_axis = {
+            _include_task_in_metric_key(key, task): tensor
+            for task, targets_per_axis in targets_per_task_per_axis.items()
+            for key, tensor in targets_per_axis.items()
+        }
+
+        MultitaskWrapper.update(
+            self, flattened_predictions_per_task_per_axis, flattened_targets_per_task_per_axis
+        )
 
 
 class LossPerAxisPerActionMetric(MultitaskWrapper):
@@ -79,6 +149,132 @@ class LossPerAxisPerActionMetric(MultitaskWrapper):
 
         for key, loss in fine_grained_loss.items():
             self.task_metrics[key](loss)
+
+
+class LossPerAxisPerActionPerTaskMetric(MultitaskWrapper):
+    """Track the loss per axis per action per task.
+
+    If there are NaN's in the loss, they get ignored. This is because of how NaN's are used to aid
+    with masking the loss during reduction.
+    """
+
+    def __init__(self) -> None:
+        # Get all of the keys that exist for the loss
+        loss_keys = [
+            _include_task_in_metric_key(key, task)
+            for key in _create_metric_key_per_axis(PER_AXIS_KEY_TEMPLATE)
+            for task in Task
+        ]
+        # And create the metric for each of them
+        metrics = {key: MeanMetric(nan_strategy="ignore") for key in loss_keys}
+        super().__init__(metrics)  # pyright: ignore[reportGeneralTypeIssues]
+
+    def update(  # type: ignore[override]
+        self, fine_grained_loss: dict[str, torch.Tensor], *, tasks: list[Task]
+    ) -> None:
+        """Update the metric with the result of a batch."""
+        if self.task_metrics.keys() != fine_grained_loss.keys():
+            raise ValueError(
+                f"Keys for loss ({fine_grained_loss.keys()}) do not match keys for metrics "
+                f"({self.task_metrics.keys()})"
+            )
+
+        # Split each tensor within each dictionary into a list of tensors, one for each task. We
+        # are splitting across the batch dimension.
+        loss_per_task_per_axis = {
+            task: {key: tensor[idx] for key, tensor in fine_grained_loss.items()}
+            for idx, task in enumerate(tasks)
+        }
+
+        # Reshape the dict into a single dict, with the task as part of the key
+        flattened_loss_per_task_per_axis = {
+            _include_task_in_metric_key(key, task): tensor
+            for task, loss_per_axis in loss_per_task_per_axis.items()
+            for key, tensor in loss_per_axis.items()
+        }
+
+        for metric_key, loss in flattened_loss_per_task_per_axis.items():
+            self.task_metrics[metric_key](loss)
+
+
+class TrainingMetrics(torch.nn.Module):
+    """Track and compute metrics for the training."""
+
+    def __init__(self, max_num_classes: int, ignore_index: int) -> None:
+        super().__init__()
+        self.pose_accuracy_per_axis = PoseAccuracyPerAxisMetric(
+            max_num_classes=max_num_classes, ignore_index=ignore_index
+        )
+        self.pose_accuracy_per_axis_per_task = PoseAccuracyPerAxisPerTaskMetric(
+            max_num_classes=max_num_classes, ignore_index=ignore_index
+        )
+        self.loss_per_axis_per_action = LossPerAxisPerActionMetric()
+        self.loss_per_axis_per_action_per_task = LossPerAxisPerActionPerTaskMetric()
+        self.examples_seen = SumMetric(compute_on_cpu=True)
+
+    @torch.no_grad()
+    def update_accuracy(
+        self,
+        predictions: dict[PoseActionType, MultiCategorical],
+        targets: dict[PoseActionType, torch.Tensor],
+        *,
+        tasks: list[Task],
+    ) -> None:
+        """Update the accuracy metrics."""
+        predicted_action_tokens: dict[PoseActionType, torch.Tensor] = {
+            pose_action_type: action_distribution.mode()
+            for pose_action_type, action_distribution in predictions.items()
+        }
+        self.pose_accuracy_per_axis.update(predicted_action_tokens, targets)
+        self.pose_accuracy_per_axis_per_task.update(predicted_action_tokens, targets, tasks=tasks)
+
+    @torch.no_grad()
+    def update_loss(
+        self, fine_grained_loss: dict[str, torch.Tensor], *, tasks: list[Task]
+    ) -> None:
+        """Update the loss metrics."""
+        self.loss_per_axis_per_action.update(fine_grained_loss)
+        self.loss_per_axis_per_action_per_task.update(fine_grained_loss, tasks=tasks)
+
+    @torch.no_grad()
+    def update_examples_seen(self, batch_size: int) -> None:
+        """Update the number of examples seen."""
+        self.examples_seen(batch_size)
+
+    @torch.no_grad()
+    def compute(self, split: Literal["train", "val"]) -> dict[str, torch.Tensor]:
+        """Compute the metrics, returning a flattened dict of all metrics."""
+        accuracy_metrics = {
+            f"{split}_{metric_key}_acc": acc
+            for metric_key, acc in {
+                **self.pose_accuracy_per_axis.compute(),
+                **self.pose_accuracy_per_axis_per_task.compute(),
+            }.items()
+        }
+
+        loss_metrics = {
+            f"{split}_{metric_key}_loss": loss
+            for metric_key, loss in {
+                **self.loss_per_axis_per_action.compute(),
+                **self.loss_per_axis_per_action_per_task.compute(),
+            }.items()
+        }
+
+        metrics = {**accuracy_metrics, **loss_metrics}
+
+        # We only want to return the examples seen metric during training since it doesn't matter
+        # during validation and shouldn't change.
+        if split == "train":
+            metrics["trainer/examples_seen"] = self.examples_seen.compute()
+
+        return metrics
+
+    def reset(self) -> None:
+        """Reset all of the metrics."""
+        self.pose_accuracy_per_axis.reset()
+        self.pose_accuracy_per_axis_per_task.reset()
+        self.loss_per_axis_per_action.reset()
+        self.loss_per_axis_per_action_per_task.reset()
 
 
 class EvaluationMetrics:
