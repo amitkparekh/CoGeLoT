ae7f6e1fc2e510d2c5524aca2cd731749de6710a
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-09-29 15:24:50 +0100

    feat: update slurm scripts for parsing the original dataset

--- scripts/slurm/parse-original-dataset.sh
+++ scripts/slurm/parse-original-dataset.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=5
-#SBATCH --mem=100G
+#SBATCH --mem=50G
 #SBATCH --time=12:00:00
 #SBATCH --partition=nodes
 #SBATCH --job-name=parse-original-dataset
-#SBATCH --output=jobs/%x.%A-%a.out
+#SBATCH --output=jobs/%x/%A-%a.out
 #SBATCH --array=0-16
 
 pdm run python -m cogelot parse-original-dataset --num-workers 15 --task-index-filter "${SLURM_ARRAY_TASK_ID}"
