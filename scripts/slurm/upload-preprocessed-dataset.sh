1408afbcf294cecc36b7f50f9a82afe466486071
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-10-02 09:56:27 +0100

    chore: update output log for slurm script

--- scripts/slurm/upload-preprocessed-dataset.sh
+++ scripts/slurm/upload-preprocessed-dataset.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --partition=nodes
 #SBATCH --job-name=upload-preprocessed-dataset
-#SBATCH --output=jobs/%x.%J.out
+#SBATCH --output=jobs/%x/%J.out
 
 export DATASETS_VERBOSITY=info
 export HUGGINGFACE_HUB_VERBOSITY=info
