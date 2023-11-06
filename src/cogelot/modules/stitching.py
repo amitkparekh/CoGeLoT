329e6b9261cf075fb16ba4fecc19a07234b0ce38
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-06 12:37:37 +0000

    feat: support stitching when decoding autoregressive action tokens

--- src/cogelot/modules/stitching.py
+++ src/cogelot/modules/stitching.py
@@ -1,142 +1,213 @@
+import itertools
 from typing import Protocol
 
 import torch
-from einops import rearrange
+from einops import rearrange, repeat
 
 
-def get_max_num_objects_from_encoded_observations(encoded_observations: torch.Tensor) -> int:
-    """Get the maximum number of objects from the encoded observations."""
-    return encoded_observations.shape[-2]
-
-
-def create_indices_for_observation_scatter(
-    observation_seq_len: int,
-    max_objects_per_observation: int,
-    device: torch.device,
-) -> torch.Tensor:
-    """Create indices that are used for the scatter operation when stitching.
-
-    This basically creates a bunch of index numbers which are used to allocate each number from the
-    observation to the stitched tensor.
-
-    Using the scatter operation is better because it removes the for-loop, which can easily add
-    time.
-    """
-    # Create an index from 0 to the total number of objects in the observations
-    obj_index = torch.arange(max_objects_per_observation * observation_seq_len, device=device)
-    # Assign the observation index to each and every object
-    obs_index = (
-        torch.arange(observation_seq_len, device=device)
-        .repeat(max_objects_per_observation, 1)
-        .T.flatten()
-    )
-    # When we add them together, we get the new index position for each object observed
-    new_obj_index = obj_index + obs_index
-    return new_obj_index
-
-
-class AddObservationsToTokensFn(Protocol):
-    """Protocol for the fn to add observations to the tokens."""
-
-    def __call__(
-        self,
-        *,
-        encoded_observations: torch.Tensor,
-        encoded_observations_mask: torch.Tensor,
-        observation_seq_len: int,
-        max_objects: int,
-        tokens: torch.Tensor,
-        masks: torch.Tensor,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        """Add the encoded observations to the token placeholders."""
-        ...  # noqa: WPS428
-
-
-def add_observations_to_tokens_using_loop(
-    *,
-    encoded_observations: torch.Tensor,
-    encoded_observations_mask: torch.Tensor,
-    observation_seq_len: int,  # noqa: ARG001
-    max_objects: int,
-    tokens: torch.Tensor,
-    masks: torch.Tensor,
-) -> tuple[torch.Tensor, torch.Tensor]:
-    """FIll in the encoded observations and masks into the correct position using for-loop.
-
-    This is slower but is kept for testing.
-    """
-    for obj_idx in range(max_objects):
-        tokens[obj_idx :: max_objects + 1] = encoded_observations[  # noqa: WPS362
-            obj_idx::max_objects
-        ]
-        masks[obj_idx :: max_objects + 1] = encoded_observations_mask[  # noqa: WPS362
-            obj_idx::max_objects
-        ]
-
-    return tokens, masks
-
-
-def add_observations_to_tokens_using_scatter(
-    *,
-    encoded_observations: torch.Tensor,
-    encoded_observations_mask: torch.Tensor,
-    observation_seq_len: int,
-    max_objects: int,
-    tokens: torch.Tensor,
-    masks: torch.Tensor,
-) -> tuple[torch.Tensor, torch.Tensor]:
-    """Fill in the encoded observations and masks into the correct positions.
-
-    For the observations, we use `torch.scatter` to fill them in so that we don't need to use
-    subscript slicing with a for-loop.
-    """
-    new_obj_index_for_scatter = create_indices_for_observation_scatter(
-        observation_seq_len,
-        max_objects,
-        device=encoded_observations.device,
-    )
-    # Shape (obj_seq_len, batch_size, dim)
-    observation_scatter_indices = new_obj_index_for_scatter.repeat(
-        *encoded_observations.shape[:0:-1], 1
-    ).transpose(-1, 0)
-    # Shape (obj_seq_len, batch_size)
-    mask_scatter_indices = new_obj_index_for_scatter.repeat(
-        *encoded_observations_mask.shape[:0:-1], 1
-    ).T
-    tokens = torch.scatter(tokens, 0, observation_scatter_indices, encoded_observations)
-    masks = torch.scatter(masks, 0, mask_scatter_indices, encoded_observations_mask)
-    return tokens, masks
-
-
-def stitch_observations_with_actions(  # noqa: WPS210
+def stitch_observations_with_actions_using_loops(
     encoded_observations: torch.Tensor,
     encoded_observations_mask: torch.Tensor,
     encoded_actions: torch.Tensor | None,
     encoded_actions_mask: torch.Tensor | None,
     *,
-    add_observations_to_tokens_fn: AddObservationsToTokensFn,
+    num_action_tokens_per_timestep: int,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    """Stitch the observations together with actions for decoder input."""
-    embed_dim = encoded_observations.size(-1)
-    batch_size, observation_seq_len = encoded_observations.shape[:2]
+    """Stitch the observations together with actions using zips and loops.
+
+    This is a lot slower but incredible useful to make sure that the other, more faster, methods
+    work as intended.
+    """
+    _, observation_seq_len, max_objects, _ = encoded_observations.shape
     actions_seq_len = 0 if encoded_actions is None else encoded_actions.shape[1]
+    total_seq_len = (
+        observation_seq_len * max_objects + actions_seq_len * num_action_tokens_per_timestep
+    )
 
     if observation_seq_len not in {actions_seq_len, actions_seq_len + 1}:
         raise AssertionError(
             "The number of observations must be equal to or one more than the number of actions"
         )
 
-    max_objects = get_max_num_objects_from_encoded_observations(encoded_observations)
-    total_seq_len = observation_seq_len * max_objects + actions_seq_len
+    observation_per_timestep = encoded_observations.split(1, dim=1)
+    observation_mask_per_timestep = encoded_observations_mask.split(1, dim=1)
+
+    actions_per_timestep = encoded_actions.split(1, dim=1) if encoded_actions is not None else []
+    actions_mask_per_timestep = (
+        encoded_actions_mask.split(1, dim=1) if encoded_actions_mask is not None else []
+    )
+
+    token_tensors_list = list(
+        itertools.chain.from_iterable(
+            itertools.zip_longest(observation_per_timestep, actions_per_timestep)
+        )
+    )
+    masks_tensors_list = list(
+        itertools.chain.from_iterable(
+            itertools.zip_longest(observation_mask_per_timestep, actions_mask_per_timestep)
+        )
+    )
+
+    token_tensors_list = [tensor for tensor in token_tensors_list if tensor is not None]
+    masks_tensors_list = [tensor for tensor in masks_tensors_list if tensor is not None]
+
+    tokens = torch.cat(token_tensors_list, dim=2).squeeze(1)
+    masks = torch.cat(masks_tensors_list, dim=2).squeeze(1)
+    assert tokens.size(1) == total_seq_len
+    assert masks.size(1) == total_seq_len
+
+    return tokens, masks
+
+
+def create_indices_for_scatter(
+    num_timesteps: int,
+    num_tokens_for_encoding_type: int,
+    index_offset_for_encoding_type: int,
+    num_tokens_per_timestep: int,
+    device: torch.device,
+) -> torch.Tensor:
+    """Create indices that are used for the scatter operation when stitching.
+
+    This basically creates a bunch of index numbers which are used to allocate each number from the
+    timestep to the stitched tensor.
+
+    Using the scatter operation is better because it removes the for-loop, which can easily add
+    time.
+    """
+    # Create an index from 0 to the number of tokens for every timestep
+    token_index = torch.arange(num_tokens_for_encoding_type * num_timesteps, device=device).view(
+        -1, num_tokens_for_encoding_type
+    )
+    # Assign the timestep index to each and every grouping of tokens
+    timestep_index = (
+        torch.arange(num_timesteps, device=device).repeat(num_tokens_for_encoding_type, 1).T
+    )
+    # Each token per timestep needs offsetting by a constant so they're in the right position
+    timestep_offset_index = timestep_index * (
+        num_tokens_per_timestep - num_tokens_for_encoding_type
+    )
+    # When we add them together, we get the new index position for each token timestep observed
+    new_index = token_index + timestep_offset_index + index_offset_for_encoding_type
+    # And then flatten it to 1D
+    return new_index.flatten()
+
+
+class AddEncodingToTokensFn(Protocol):
+    """Protocol for the fn to stitch together the encodings to the tokens.
+
+    This is so that the observations and actions can be interleaved efficiently.
+    """
+
+    def __call__(
+        self,
+        *,
+        encoding: torch.Tensor,
+        encoding_mask: torch.Tensor,
+        num_tokens_per_timestep: int,
+        index_offset_for_encoding_type: int,
+        tokens: torch.Tensor,
+        masks: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """Add the encoded information to the token placeholders.
+
+        The `num_tokens_per_timestep` are the total number of tokens across all the various
+        information types.
+        """
+        ...
+
+
+def add_encoding_to_tokens_using_subslice_loop(
+    *,
+    encoding: torch.Tensor,
+    encoding_mask: torch.Tensor,
+    num_tokens_per_timestep: int,
+    index_offset_for_encoding_type: int,
+    tokens: torch.Tensor,
+    masks: torch.Tensor,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Add the encoded information and masks into the right positions using for-loop.
+
+    This is slower but is kept for testing.
+    """
+    # Get the information from the shapes
+    num_tokens_for_encoding_type = encoding.size(2)
 
     # Rearrange the tensors to be in the right structure
-    # embedded_observations = rearrange(embedded_observations, "L B Q E -> B L Q E")
-    encoded_observations = rearrange(encoded_observations, "B L Q E -> B (L Q) E")
-    encoded_observations = rearrange(encoded_observations, "B L E -> L B E")
+    encoding = rearrange(encoding, "B L Q E -> (L Q) B E")
+    encoding_mask = rearrange(encoding_mask, "B L Q -> (L Q) B")
 
-    # embedded_observations_mask = rearrange(embedded_observations_mask, "L B Q -> B L Q")
-    encoded_observations_mask = rearrange(encoded_observations_mask, "B L Q -> B (L Q)")
-    encoded_observations_mask = rearrange(encoded_observations_mask, "B L -> L B")
+    for token_idx in range(num_tokens_for_encoding_type):
+        output_start_index = token_idx + index_offset_for_encoding_type
+        tokens[output_start_index::num_tokens_per_timestep] = encoding[
+            token_idx::num_tokens_for_encoding_type
+        ]
+        masks[output_start_index::num_tokens_per_timestep] = encoding_mask[
+            token_idx::num_tokens_for_encoding_type
+        ]
+
+    return tokens, masks
+
+
+def add_encoding_to_tokens_using_scatter(
+    *,
+    encoding: torch.Tensor,
+    encoding_mask: torch.Tensor,
+    num_tokens_per_timestep: int,
+    index_offset_for_encoding_type: int,
+    tokens: torch.Tensor,
+    masks: torch.Tensor,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Fill in the encoded information and masks into the correct positions.
+
+    Use `torch.scatter` to fill them in so that we don't need to use subscript slicing with a for-loop.
+    """
+    # Get the information from the shapes
+    num_timesteps, num_tokens_for_encoding_type = encoding.shape[1:3]
+
+    # Rearrange the tensors to be in the right structure
+    encoding = rearrange(encoding, "B L Q E -> (L Q) B E")
+    encoding_mask = rearrange(encoding_mask, "B L Q -> (L Q) B")
+
+    new_token_index_for_scatter = create_indices_for_scatter(
+        num_timesteps,
+        num_tokens_for_encoding_type,
+        index_offset_for_encoding_type,
+        num_tokens_per_timestep,
+        device=encoding.device,
+    )
+
+    encoding_scatter_indices = repeat(
+        new_token_index_for_scatter, "i -> i bsz dim", bsz=encoding.size(1), dim=encoding.size(2)
+    )
+    mask_scatter_indices = repeat(
+        new_token_index_for_scatter, "i -> i bsz", bsz=encoding_mask.size(1)
+    )
+
+    tokens = torch.scatter(tokens, 0, encoding_scatter_indices, encoding)
+    masks = torch.scatter(masks, 0, mask_scatter_indices, encoding_mask)
+    return tokens, masks
+
+
+def stitch_observations_with_actions(
+    encoded_observations: torch.Tensor,
+    encoded_observations_mask: torch.Tensor,
+    encoded_actions: torch.Tensor | None,
+    encoded_actions_mask: torch.Tensor | None,
+    *,
+    add_encoding_to_tokens_fn: AddEncodingToTokensFn,
+    num_action_tokens_per_timestep: int,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Stitch the observations together with actions for decoder input."""
+    batch_size, observation_seq_len, max_objects, embed_dim = encoded_observations.shape
+    actions_seq_len = 0 if encoded_actions is None else encoded_actions.shape[1]
+    total_seq_len = (
+        observation_seq_len * max_objects + actions_seq_len * num_action_tokens_per_timestep
+    )
+
+    if observation_seq_len not in {actions_seq_len, actions_seq_len + 1}:
+        raise AssertionError(
+            "The number of observations must be equal to or one more than the number of actions"
+        )
 
     # Create tensors which will we will use to put the various tokens into
     tokens = torch.empty(
@@ -152,19 +223,26 @@ def stitch_observations_with_actions(  # noqa: WPS210
         total_seq_len, batch_size, dtype=torch.bool, device=encoded_observations.device
     )
 
-    tokens, masks = add_observations_to_tokens_fn(
-        encoded_observations=encoded_observations,
-        encoded_observations_mask=encoded_observations_mask,
-        observation_seq_len=observation_seq_len,
-        max_objects=max_objects,
+    # Add the observations to the tokens and the masks
+    tokens, masks = add_encoding_to_tokens_fn(
+        encoding=encoded_observations,
+        encoding_mask=encoded_observations_mask,
+        num_tokens_per_timestep=max_objects + num_action_tokens_per_timestep,
+        index_offset_for_encoding_type=0,
         tokens=tokens,
         masks=masks,
     )
 
-    if encoded_actions is not None:
-        tokens[max_objects :: max_objects + 1] = encoded_actions.transpose(0, 1)  # noqa: WPS362
-    if encoded_actions_mask is not None:
-        masks[max_objects :: max_objects + 1] = encoded_actions_mask.T  # noqa: WPS362
+    # If we have actions, then we add them in too
+    if encoded_actions is not None and encoded_actions_mask is not None:
+        tokens, masks = add_encoding_to_tokens_fn(
+            encoding=encoded_actions,
+            encoding_mask=encoded_actions_mask,
+            num_tokens_per_timestep=max_objects + num_action_tokens_per_timestep,
+            index_offset_for_encoding_type=max_objects,
+            tokens=tokens,
+            masks=masks,
+        )
 
     # Put the batch first
     tokens = rearrange(tokens, "L B E -> B L E")
---
+++ tests/test_stitching.py
@@ -0,0 +1,157 @@
+import torch
+from pytest_cases import fixture, parametrize
+
+from cogelot.data.collate import collate_preprocessed_instances
+from cogelot.models.training import VIMALightningModule
+from cogelot.modules.stitching import (
+    add_encoding_to_tokens_using_scatter,
+    add_encoding_to_tokens_using_subslice_loop,
+    stitch_observations_with_actions,
+    stitch_observations_with_actions_using_loops,
+)
+from cogelot.structures.model import ModelInstance, PreprocessedInstance
+
+
+@fixture(scope="module")
+@parametrize(
+    "make_num_actions_less_than_obs",
+    [False, True],
+    ids=["num_actions_equal_obs", "num_actions_less_than_obs"],
+)
+@parametrize(
+    "max_num_timesteps",
+    [1, 2, None],
+    ids=["max_timesteps=1", "max_timesteps=2", "max_timesteps=None"],
+)
+def embedded_inputs(
+    vima_lightning_module: VIMALightningModule,
+    all_preprocessed_instances: list[PreprocessedInstance],
+    max_num_timesteps: int | None,
+    make_num_actions_less_than_obs: bool,  # noqa: FBT001
+) -> ModelInstance:
+    batch = collate_preprocessed_instances(all_preprocessed_instances)
+    embedded_inputs = vima_lightning_module.embed_inputs(batch)
+    assert embedded_inputs.encoded_actions is not None
+    assert embedded_inputs.encoded_actions_mask is not None
+
+    encoded_observations = embedded_inputs.encoded_observations
+    encoded_observations_mask = embedded_inputs.encoded_observations_mask
+    encoded_actions = embedded_inputs.encoded_actions
+    encoded_actions_mask = embedded_inputs.encoded_actions_mask
+
+    # Make sure the number of timesteps are identical for observations and actions
+    assert encoded_actions.size(1) == encoded_observations_mask.size(1)
+    current_timesteps = encoded_observations.size(1)
+
+    # If desired, limit the total number of timesteps included
+    if max_num_timesteps is not None and max_num_timesteps < current_timesteps:
+        encoded_observations = encoded_observations[:, :max_num_timesteps]
+        encoded_observations_mask = encoded_observations_mask[:, :max_num_timesteps]
+        encoded_actions = encoded_actions[:, :max_num_timesteps]
+        encoded_actions_mask = encoded_actions_mask[:, :max_num_timesteps]
+
+    # Get the current num timesteps in the obs and actions
+    num_observation_timesteps = encoded_observations.size(1)
+    num_action_timesteps = encoded_actions.size(1)
+    assert num_observation_timesteps == num_action_timesteps
+
+    # If desired, make the number of actions less than the number of observations
+    if make_num_actions_less_than_obs:
+        num_action_timesteps = num_observation_timesteps - 1
+
+        if num_action_timesteps > 0:
+            encoded_actions = encoded_actions[:, :num_action_timesteps]
+            encoded_actions_mask = encoded_actions_mask[:, :num_action_timesteps]
+        else:
+            encoded_actions = None
+            encoded_actions_mask = None
+
+    return ModelInstance(
+        encoded_prompt=embedded_inputs.encoded_prompt,
+        encoded_prompt_mask=embedded_inputs.encoded_prompt_mask,
+        encoded_observations=encoded_observations,
+        encoded_observations_mask=encoded_observations_mask,
+        encoded_actions=encoded_actions,
+        encoded_actions_mask=encoded_actions_mask,
+    )
+
+
+@fixture
+def num_action_tokens_per_timestep(embedded_inputs: ModelInstance) -> int:
+    return (
+        embedded_inputs.encoded_actions_mask.size(-1)
+        if embedded_inputs.encoded_actions_mask is not None
+        else 1
+    )
+
+
+def test_stitching_with_zip_loops_works(
+    embedded_inputs: ModelInstance, num_action_tokens_per_timestep: int
+) -> None:
+    expected_tokens, expected_mask = stitch_observations_with_actions_using_loops(
+        embedded_inputs.encoded_observations,
+        embedded_inputs.encoded_observations_mask,
+        embedded_inputs.encoded_actions,
+        embedded_inputs.encoded_actions_mask,
+        num_action_tokens_per_timestep=num_action_tokens_per_timestep,
+    )
+
+    assert expected_tokens is not None
+    assert expected_mask is not None
+
+    assert expected_tokens.ndim == 3
+    assert expected_mask.ndim == 2
+
+    assert expected_tokens.size(0) == len(embedded_inputs.encoded_prompt)
+    assert expected_mask.size(0) == len(embedded_inputs.encoded_prompt)
+
+    assert expected_tokens.size(1) == expected_mask.size(1)
+
+
+def test_stitching_with_subslice_loops_is_correct(
+    embedded_inputs: ModelInstance, num_action_tokens_per_timestep: int
+) -> None:
+    """Test that stitching the VIMA way works correctly."""
+    expected_tokens, expected_mask = stitch_observations_with_actions_using_loops(
+        embedded_inputs.encoded_observations,
+        embedded_inputs.encoded_observations_mask,
+        embedded_inputs.encoded_actions,
+        embedded_inputs.encoded_actions_mask,
+        num_action_tokens_per_timestep=num_action_tokens_per_timestep,
+    )
+
+    actual_tokens, actual_mask = stitch_observations_with_actions(
+        embedded_inputs.encoded_observations,
+        embedded_inputs.encoded_observations_mask,
+        embedded_inputs.encoded_actions,
+        embedded_inputs.encoded_actions_mask,
+        add_encoding_to_tokens_fn=add_encoding_to_tokens_using_subslice_loop,
+        num_action_tokens_per_timestep=num_action_tokens_per_timestep,
+    )
+
+    assert torch.all(expected_tokens == actual_tokens)
+    assert torch.all(actual_mask == expected_mask)
+
+
+def test_stitching_with_scatter_is_correct(
+    embedded_inputs: ModelInstance, num_action_tokens_per_timestep: int
+) -> None:
+    expected_tokens, expected_mask = stitch_observations_with_actions_using_loops(
+        embedded_inputs.encoded_observations,
+        embedded_inputs.encoded_observations_mask,
+        embedded_inputs.encoded_actions,
+        embedded_inputs.encoded_actions_mask,
+        num_action_tokens_per_timestep=num_action_tokens_per_timestep,
+    )
+
+    actual_tokens, actual_mask = stitch_observations_with_actions(
+        embedded_inputs.encoded_observations,
+        embedded_inputs.encoded_observations_mask,
+        embedded_inputs.encoded_actions,
+        embedded_inputs.encoded_actions_mask,
+        add_encoding_to_tokens_fn=add_encoding_to_tokens_using_scatter,
+        num_action_tokens_per_timestep=num_action_tokens_per_timestep,
+    )
+
+    assert torch.all(expected_tokens == actual_tokens)
+    assert torch.all(actual_mask == expected_mask)
