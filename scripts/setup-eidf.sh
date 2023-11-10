d6839cb51c92e4ce25acf8ab6158188e638ecac2
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-10 17:28:48 +0000

    chore(deps): move the environment deps to the main group

--- scripts/setup-eidf.sh
+++ scripts/setup-eidf.sh
@@ -6,6 +6,8 @@ mkdir -p /mnt/ceph_rbd/wandb /mnt/ceph_rbd/huggingface /mnt/ceph_rbd/data /mnt/c
 # Install deps (without torch since we are using the conda one)
 pdm export --prod --without-hashes | grep -v "torch==" >requirements.txt
 pip install --no-deps -r requirements.txt
+pip install -e .
+python -m torch.utils.collect_env
 
 # Add a symlink to the mnt for storage/data, since that's where everything goes
 ln -s /mnt/ceph_rbd/data ./storage/
