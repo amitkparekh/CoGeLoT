b831b7c0e50c158ae89fe3ece7ca904ed1868aed
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-10 18:30:49 +0000

    fix(scripts): setup eidf script

--- scripts/setup-eidf.sh
+++ scripts/setup-eidf.sh
@@ -3,11 +3,15 @@
 # Make the various directories we'll need
 mkdir -p /mnt/ceph_rbd/wandb /mnt/ceph_rbd/huggingface /mnt/ceph_rbd/data /mnt/ceph_rbd/torch
 
+apt update -y
+apt install -y --no-install-recommends gcc g++
+rm -rf /var/lib/apt/lists/*
+
+# Add a symlink to the mnt for storage/data, since that's where everything goes
+ln -s /mnt/ceph_rbd/data ./storage/
+
 # Install deps (without torch since we are using the conda one)
 pdm export --prod --without-hashes | grep -v "torch==" >requirements.txt
 pip install --no-deps -r requirements.txt
 pip install --no-deps -e .
 python -m torch.utils.collect_env
-
-# Add a symlink to the mnt for storage/data, since that's where everything goes
-ln -s /mnt/ceph_rbd/data ./storage/
