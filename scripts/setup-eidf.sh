a263043dd593cef5ee793e389fb97903eeb04b34
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-10 16:45:39 +0000

    chore: add setup script for eidf

---
+++ scripts/setup-eidf.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Make the various directories we'll need
+mkdir -p /mnt/ceph_rbd/wandb /mnt/ceph_rbd/huggingface /mnt/ceph_rbd/data /mnt/ceph_rbd/torch
+
+# Install deps (without torch since we are using the conda one)
+pdm export --prod --without-hashes | grep -v "torch==" >requirements.txt
+pip install -r requirements.txt
+
+# Add a symlink to the mnt for storage/data, since that's where everything goes
+ln -s /mnt/ceph_rbd/data ./storage/
