963f75c7696225792813b527aa8b63d9c0218f99
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-10 17:17:10 +0000

    fix(scripts): do not let pip install torch at all within eidf
    we are using conda's torch

--- scripts/setup-eidf.sh
+++ scripts/setup-eidf.sh
@@ -5,7 +5,7 @@ mkdir -p /mnt/ceph_rbd/wandb /mnt/ceph_rbd/huggingface /mnt/ceph_rbd/data /mnt/c
 
 # Install deps (without torch since we are using the conda one)
 pdm export --prod --without-hashes | grep -v "torch==" >requirements.txt
-pip install -r requirements.txt
+pip install --no-deps -r requirements.txt
 
 # Add a symlink to the mnt for storage/data, since that's where everything goes
 ln -s /mnt/ceph_rbd/data ./storage/
