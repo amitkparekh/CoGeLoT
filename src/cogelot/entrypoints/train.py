de5065b3fec090e31182f8d9ab7bc7d75559b55c
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-11 15:57:49 +0000

    fix: go back to the old way of logging hparams

--- src/cogelot/entrypoints/train.py
+++ src/cogelot/entrypoints/train.py
@@ -15,13 +15,7 @@ def train_model(config: DictConfig) -> None:
     """Run the training."""
     datamodule, model, trainer = instantiate_modules_from_hydra(config)
 
-    # Manually update the hparams with the flattened config, since calling `save_hyperparameters`
-    # wants to follow all of the frame stack, which then causing breaking as I'm using typer to run
-    # things. So I've made things harder for myself by wanting to use a library.
-    model._log_hyperparams = True  # noqa: SLF001
-    model._set_hparams(flatten_config(config))  # noqa: SLF001
-    for model_logger in model.loggers:
-        model_logger.log_hyperparams(flatten_config(config))
+    model.save_hyperparameters(flatten_config(config))
 
     logger.info("Starting training...")
     trainer.fit(model, datamodule=datamodule)
