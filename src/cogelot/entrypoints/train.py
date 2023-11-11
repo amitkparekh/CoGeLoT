2efe359decdbd8b13933dec45b268d625d492354
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-11 16:08:32 +0000

    fix: try to send the config to wandb using the module

--- src/cogelot/entrypoints/train.py
+++ src/cogelot/entrypoints/train.py
@@ -10,12 +10,21 @@ from cogelot.common.hydra import instantiate_modules_from_hydra
 CONFIG_DIR = Path.cwd().joinpath("configs").as_posix()
 
 
+def send_config_to_wandb(config: DictConfig) -> None:
+    """Send the config to wandb using the wandb module."""
+    import wandb
+
+    wandb.config.update(flatten_config(config))
+    wandb.config.update()
+
+
 @hydra.main(config_path=CONFIG_DIR, config_name="train.yaml", version_base="1.3")
 def train_model(config: DictConfig) -> None:
     """Run the training."""
     datamodule, model, trainer = instantiate_modules_from_hydra(config)
 
     model.save_hyperparameters(flatten_config(config))
+    send_config_to_wandb(config)
 
     logger.info("Starting training...")
     trainer.fit(model, datamodule=datamodule)
