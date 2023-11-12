887668ddca83901141cc06a6d3a1890ad6c13916
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-12 15:38:10 +0000

    feat: reduce default precision for torch

--- src/cogelot/entrypoints/train.py
+++ src/cogelot/entrypoints/train.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import hydra
+import torch
 from loguru import logger
 from omegaconf import DictConfig
 
@@ -13,6 +14,10 @@ CONFIG_DIR = Path.cwd().joinpath("configs").as_posix()
 @hydra.main(config_path=CONFIG_DIR, config_name="train.yaml", version_base="1.3")
 def train_model(config: DictConfig) -> None:
     """Run the training."""
+    # Set the internal precision to be slightly lower than the default
+    # https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
+    torch.set_float32_matmul_precision("high")
+
     datamodule, model, trainer = instantiate_modules_from_hydra(config)
 
     model.save_hyperparameters(flatten_config(config))
