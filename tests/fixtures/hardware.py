6b54e800e4d535f30e1d77bc3b92f760f5b7a6b9
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-11-17 18:37:49 +0000

    test: scope all gpu tests together into a single group

--- tests/fixtures/hardware.py
+++ tests/fixtures/hardware.py
@@ -8,7 +8,10 @@ torch_device = param_fixture(
         torch.device("cpu"),
         pytest.param(
             torch.device("cuda"),
-            marks=pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available"),
+            marks=[
+                pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA not available"),
+                pytest.mark.xdist_group("gpu"),
+            ],
         ),
     ],
     ids=["cpu", "cuda"],
