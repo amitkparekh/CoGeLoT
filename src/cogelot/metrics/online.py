3426d385a87e490b13122fd9abc342b19ddfa384
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-12-28 13:36:20 +0000

    feat(evaluation): create hestitance and flailing metrics

--- src/cogelot/metrics/online.py
+++ src/cogelot/metrics/online.py
@@ -1,3 +1,4 @@
+import math
 from typing import ClassVar
 
 import torch
@@ -8,21 +9,49 @@ from torchmetrics import MeanMetric, SumMetric
 from cogelot.structures.vima import Partition, Task, get_task_group_from_task
 
 
-class SuccessWobbleMetric(MeanMetric):
-    """Track the robot confidence after the first success.
+def compute_hesitance(success_tracker_per_step: list[bool]) -> float:
+    """Compute the hesitance of an episode from the tracked success statuses.
 
-    High value is good.
+    Hesitance is calucated as the ratio of the False's that come after the first True.
+
+    If hesitance is ever < 1, then it means that the model succeeded at some point, but decided
+    against it.
     """
+    first_success_index = success_tracker_per_step.index(True)
+    # +1 beacuse we don't want to include the first success
+    tracker_after_first_success = success_tracker_per_step[first_success_index + 1 :]
 
-    def update(self, success_tracker_per_step: list[bool]) -> None:  # type: ignore[override]
-        """Update the metric with the result of an episode."""
-        first_success_index = success_tracker_per_step.index(True)
-        confidence_after_first_success = success_tracker_per_step[first_success_index:]
+    try:
+        hesitance = tracker_after_first_success.count(False) / len(tracker_after_first_success)
+    except ZeroDivisionError:
+        hesitance = 0
 
-        average_after_first_success = confidence_after_first_success.count(True) / len(
-            confidence_after_first_success
-        )
-        super().update(average_after_first_success)
+    return hesitance
+
+
+def compute_flailing(success_tracker_per_step: list[bool]) -> float:
+    """Compute how much the robot flailed in an episode from the tracked success statuses.
+
+    Flailing is caluclated from the number of steps AFTER the first success.
+
+    Flailing has two parts: just waving around like nothing, and causing harm to progress. We are
+    just tracking the first part, which is how long the robot continues to make steps after
+    the first success. The longer the steps, the worst it is.
+
+    f(x) = tanh(ln(x)), therefore f is bounded by [0, 1] for any x > 0. In case the number of steps
+    is 0, we just return 0. This function is incredibly strict, as flailing for 2 steps returns a
+    value of 0.6.
+    """
+    first_success_index = success_tracker_per_step.index(True)
+    # +1 beacuse we don't want to include the first success
+    tracker_after_first_success = success_tracker_per_step[first_success_index + 1 :]
+
+    num_steps_flailing = len(tracker_after_first_success)
+    try:
+        flail_value = math.tanh(math.log(num_steps_flailing))
+    except ValueError:
+        flail_value = 0
+    return flail_value
 
 
 class OnlineEvaluationMetrics:
@@ -45,8 +74,11 @@ class OnlineEvaluationMetrics:
         self.success_rate = {
             partition: {task: MeanMetric() for task in Task} for partition in Partition
         }
-        self.success_wobble_rate = {
-            partition: {task: SuccessWobbleMetric() for task in Task} for partition in Partition
+        self.hesitance_rate = {
+            partition: {task: MeanMetric() for task in Task} for partition in Partition
+        }
+        self.flailing_rate = {
+            partition: {task: MeanMetric() for task in Task} for partition in Partition
         }
         self.steps_taken = {
             partition: {task: MeanMetric() for task in Task} for partition in Partition
@@ -68,11 +100,12 @@ class OnlineEvaluationMetrics:
         """Update the metric with the result of an episode."""
         logger.debug(f"Updating metric for {partition}/{task}")
 
-        # To be successful means it ends successfully
         is_successful = success_tracker_per_step[-1]
 
-        self.success_wobble_rate[partition][task](success_tracker_per_step)
-        self.success_rate[partition][task](int(is_successful))
+        # To be successful means it ends successfully
+        self.success_rate[partition][task](int(success_tracker_per_step[-1]))
+        self.hesitance_rate[partition][task](compute_hesitance(success_tracker_per_step))
+        self.flailing_rate[partition][task](compute_flailing(success_tracker_per_step))
         self.steps_taken[partition][task](num_steps_taken)
         self.tasks_seen[partition][task](1)
 
@@ -127,14 +160,21 @@ class OnlineEvaluationMetrics:
             if seen_per_task_per_partition[partition][task] > 0
         }
 
-        computed_success_wobble_rate = {
+        computed_hesitant_rate = {
             self.key_template.format(
-                partition=partition.value,
-                task=str(task.value + 1).zfill(2),
-                metric="success_wobble",
-            ): success_wobble_rate.compute()
-            for partition, success_wobble_rate_per_task in self.success_wobble_rate.items()
-            for task, success_wobble_rate in success_wobble_rate_per_task.items()
+                partition=partition.value, task=str(task.value + 1).zfill(2), metric="hesitant"
+            ): hesitant_rate.compute()
+            for partition, hesitant_rate_per_task in self.hesitance_rate.items()
+            for task, hesitant_rate in hesitant_rate_per_task.items()
+            if seen_per_task_per_partition[partition][task] > 0
+        }
+
+        computed_flailing_rate = {
+            self.key_template.format(
+                partition=partition.value, task=str(task.value + 1).zfill(2), metric="flailing"
+            ): flailing_rate.compute()
+            for partition, flailing_rate_per_task in self.flailing_rate.items()
+            for task, flailing_rate in flailing_rate_per_task.items()
             if seen_per_task_per_partition[partition][task] > 0
         }
 
@@ -142,5 +182,6 @@ class OnlineEvaluationMetrics:
             **computed_tasks_seen,
             **computed_steps,
             **computed_success_rate,
-            **computed_success_wobble_rate,
+            **computed_hesitant_rate,
+            **computed_flailing_rate,
         }
