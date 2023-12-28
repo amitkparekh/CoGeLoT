3d60e8f8b70557ccc2d8c67d224a063759d8f9bf
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2023-12-28 11:47:53 +0000

    feat(scripts): update dataset creation job script for both variants

--- scripts/submit-dataset-creation-jobs.sh
+++ scripts/submit-dataset-creation-jobs.sh
@@ -7,26 +7,36 @@ get_abs_filename() {
 	echo "$(cd "$(dirname "$0")" && pwd)/$1"
 }
 
-# Parse the original dataset
-parse_original_job_id=$(sbatch --parsable "$(get_abs_filename 'slurm/parse-original-dataset.sh')")
-echo "Parsing original dataset: $parse_original_job_id"
+submit_jobs() {
+	echo "Submitting jobs for dataset variant: $DATASET_VARIANT"
 
-# Create the raw HF datasets
-create_raw_dataset_job_id=$(sbatch --parsable --dependency=afterok:"${parse_original_job_id}" "$(get_abs_filename slurm/create-raw-dataset.sh)")
-echo "Create raw dataset: $create_raw_dataset_job_id"
+	# Parse the original dataset
+	parse_original_job_id=$(sbatch --export=DATASET_VARIANT=$DATASET_VARIANT --parsable "$(get_abs_filename 'slurm/parse-original-dataset.sh')")
+	echo "Parsing original dataset: $parse_original_job_id"
 
-# Preprocess all the instances
-preprocess_instances_job_id=$(sbatch --parsable --dependency=afterok:"${create_raw_dataset_job_id}" "$(get_abs_filename slurm/preprocess-instances.sh)")
-echo "Preprocess instances: $preprocess_instances_job_id"
+	# Create the raw HF datasets
+	create_raw_dataset_job_id=$(sbatch --export=DATASET_VARIANT=$DATASET_VARIANT --parsable --dependency=afterok:"${parse_original_job_id}" "$(get_abs_filename slurm/create-raw-dataset.sh)")
+	echo "Create raw dataset: $create_raw_dataset_job_id"
 
-# Create the preprocessed HF datasets
-create_preprocessed_dataset_job_id=$(sbatch --parsable --dependency=afterok:"${preprocess_instances_job_id}" "$(get_abs_filename slurm/create-preprocessed-dataset.sh)")
-echo "Create preprocessed dataset: $create_preprocessed_dataset_job_id"
+	# Preprocess all the instances
+	preprocess_instances_job_id=$(sbatch --export=DATASET_VARIANT=$DATASET_VARIANT --parsable --dependency=afterok:"${create_raw_dataset_job_id}" "$(get_abs_filename slurm/preprocess-instances.sh)")
+	echo "Preprocess instances: $preprocess_instances_job_id"
 
-# Upload the raw HF datasets (after we are done preprocessing instances)
-upload_raw_dataset_job_id=$(sbatch --parsable --dependency=afterok:"${create_preprocessed_dataset_job_id}" "$(get_abs_filename slurm/upload-raw-dataset.sh)")
-echo "Upload raw dataset: $upload_raw_dataset_job_id"
+	# Create the preprocessed HF datasets
+	create_preprocessed_dataset_job_id=$(sbatch --export=DATASET_VARIANT=$DATASET_VARIANT --parsable --dependency=afterok:"${preprocess_instances_job_id}" "$(get_abs_filename slurm/create-preprocessed-dataset.sh)")
+	echo "Create preprocessed dataset: $create_preprocessed_dataset_job_id"
 
-# Upload the preprocessed HF datasets
-upload_preprocessed_dataset_job_id=$(sbatch --parsable --dependency=afterok:"${create_preprocessed_dataset_job_id}" "$(get_abs_filename slurm/upload-preprocessed-dataset.sh)")
-echo "Upload preprocessed dataset: $upload_preprocessed_dataset_job_id"
+	# Upload the raw HF datasets (after we are done preprocessing instances)
+	upload_raw_dataset_job_id=$(sbatch --export=DATASET_VARIANT=$DATASET_VARIANT --parsable --dependency=afterok:"${preprocess_instances_job_id}" "$(get_abs_filename slurm/upload-raw-dataset.sh)")
+	echo "Upload raw dataset: $upload_raw_dataset_job_id"
+
+	# Upload the preprocessed HF datasets
+	upload_preprocessed_dataset_job_id=$(sbatch --export=DATASET_VARIANT=$DATASET_VARIANT --parsable --dependency=afterok:"${create_preprocessed_dataset_job_id}" "$(get_abs_filename slurm/upload-preprocessed-dataset.sh)")
+	echo "Upload preprocessed dataset: $upload_preprocessed_dataset_job_id"
+}
+
+DATASET_VARIANT='original'
+submit_jobs
+
+DATASET_VARIANT="keep_null_action"
+submit_jobs
