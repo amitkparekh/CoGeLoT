73e2fa3bfe9144bd71b38069bce9832ea1d7e83e
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-01-02 13:18:00 +0000

    fix: preprocess the config before instantiating with hydra

--- src/cogelot/entrypoints/train.py
+++ src/cogelot/entrypoints/train.py
@@ -6,7 +6,7 @@ from loguru import logger
 from omegaconf import DictConfig
 
 from cogelot.common.config import flatten_config
-from cogelot.common.hydra import instantiate_modules_from_hydra
+from cogelot.common.hydra import instantiate_modules_from_hydra, preprocess_config_for_hydra
 from cogelot.data.datamodule import VIMADataModule
 
 CONFIG_DIR = Path.cwd().joinpath("configs").as_posix()
@@ -15,6 +15,8 @@ CONFIG_DIR = Path.cwd().joinpath("configs").as_posix()
 @hydra.main(config_path=CONFIG_DIR, config_name="train.yaml", version_base="1.3")
 def train_model(config: DictConfig) -> None:
     """Run the training."""
+    config = preprocess_config_for_hydra(config)
+
     # Set the internal precision to be slightly lower than the default
     # https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
     torch.set_float32_matmul_precision("high")
