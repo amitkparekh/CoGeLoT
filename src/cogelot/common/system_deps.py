bd40d4b315fd7e04dfa4d5b53713882421e9a205
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-01-18 16:35:20 +0000

    feat(wandb): add fn to verify ffmpeg is available on the system
    this is needed for wandb to create videos

---
+++ src/cogelot/common/system_deps.py
@@ -0,0 +1,17 @@
+import subprocess
+from contextlib import suppress
+
+
+def verify_ffmpeg_is_available() -> None:
+    """Verify ffmpeg is available."""
+    is_available = False
+    with suppress(FileNotFoundError):
+        outcome = subprocess.run(
+            ["ffmpeg", "-version"],  # noqa: S603 S607
+            capture_output=True,
+            check=False,
+        )
+        is_available = outcome.returncode == 0
+
+    if not is_available:
+        raise RuntimeError("ffmpeg is not installed. Please install ffmpeg to run the evaluation.")
