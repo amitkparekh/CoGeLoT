4c48d504bb8a40f826a79fe6409c1f4d28236956
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-01-23 10:41:44 +0000

    fix(entrypoints): validation and evaluation runs

--- src/cogelot/entrypoints/evaluate.py
+++ src/cogelot/entrypoints/evaluate.py
@@ -1,11 +1,17 @@
+import torch
 from loguru import logger
 from omegaconf import DictConfig
 
-from cogelot.common.hydra import instantiate_modules_from_hydra
+from cogelot.common.hydra import instantiate_modules_from_hydra, preprocess_config_for_hydra
 
 
 def validate_model(config: DictConfig) -> None:
     """Run the evaluation."""
+    # Set the internal precision to be slightly lower than the default
+    # https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
+    torch.set_float32_matmul_precision("high")
+
+    config = preprocess_config_for_hydra(config)
     datamodule, model, trainer = instantiate_modules_from_hydra(config)
 
     logger.info("Starting validation...")
@@ -14,6 +20,11 @@ def validate_model(config: DictConfig) -> None:
 
 def evaluate_model(config: DictConfig) -> None:
     """Run the evaluation."""
+    # Set the internal precision to be slightly lower than the default
+    # https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_precision
+    torch.set_float32_matmul_precision("high")
+
+    config = preprocess_config_for_hydra(config)
     datamodule, model, trainer = instantiate_modules_from_hydra(config)
 
     logger.info("Starting evaluation...")
