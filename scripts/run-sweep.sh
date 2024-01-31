e9b85d24cc4f48fa09cb7c10794d9a99ad4dc8a0
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-01-31 16:08:14 +0000

    chore: update run sweep to run multiple sweeps

--- scripts/run-sweep.sh
+++ scripts/run-sweep.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-wandb agent pyop/CoGeLoT/61rabbn6
+# Their VIMA
+wandb agent pyop/CoGeLoT/6n4xekx1
+
+# PosEmb - orig dataset
+wandb agent pyop/CoGeLoT/dssf38fn
+
+# Decoder flavours - orig dataset
+wandb agent pyop/CoGeLoT/qya9qtum
