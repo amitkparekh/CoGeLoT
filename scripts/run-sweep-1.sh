af43ddbfd8b0142db7c5496fc885d4d46028f073
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-02-04 15:51:05 +0000

    fix(scripts): need to add the wait for processes to finish

--- scripts/run-sweep-1.sh
+++ scripts/run-sweep-1.sh
@@ -2,8 +2,16 @@
 
 # ALIBI orig
 CUDA_VISIBLE_DEVICES=0 wandb agent pyop/CoGeLoT/98e8cm6x &
+echo "Started agent 1: ALIBI orig"
 CUDA_VISIBLE_DEVICES=1 wandb agent pyop/CoGeLoT/98e8cm6x &
+echo "Started agent 2: ALIBI orig"
 
 # ROPE orig
 CUDA_VISIBLE_DEVICES=2 wandb agent pyop/CoGeLoT/3he2lhl7 &
+echo "Started agent 3: ROPE orig"
 CUDA_VISIBLE_DEVICES=3 wandb agent pyop/CoGeLoT/3he2lhl7 &
+echo "Started agent 4: ROPE orig"
+
+echo "Waiting for processes to finish"
+wait "$(jobs -p)"
+echo "All processes finished"
