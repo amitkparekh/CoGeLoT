c510243b2e09b46ed835b507c36fba1707a8d8ac
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-02-07 10:25:43 +0000

    fix: switch from tf to greedy when not training

--- src/cogelot/modules/policy.py
+++ src/cogelot/modules/policy.py
@@ -237,7 +237,11 @@ class Policy(torch.nn.Module):
             num_action_tokens_per_timestep=self.num_action_tokens_per_timestep,
         )
 
-        transformer_output = self._transformer_decoder(
+        transformer_decoder = (
+            self._transformer_decoder if self.training else self._transformer_decoder.generate
+        )
+
+        transformer_output = transformer_decoder(
             tgt=tokens,
             tgt_key_padding_mask=masks,
             memory=encoded_prompt,
