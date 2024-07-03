973058f200c32aa5e9fddadd03cefa3842fa7a30
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2024-07-03 15:50:56 +0100

    test: needed to add reason to skipif

--- tests/test_external_access.py
+++ tests/test_external_access.py
@@ -13,16 +13,16 @@ def hf_api() -> HfApi:
     return HfApi()
 
 
-@pytest.mark.skipif(os.environ.get("CI") is not None)
+@pytest.mark.skipif(os.environ.get("CI") is not None, reason="Skipping HF API tests in CI")
 def test_hf_token_has_write_permission(hf_api: HfApi) -> None:
     assert hf_api.get_token_permission() == "write"
 
 
-@pytest.mark.skipif(os.environ.get("CI") is not None)
+@pytest.mark.skipif(os.environ.get("CI") is not None, reason="Skipping HF API tests in CI")
 def test_can_access_hf_dataset_repo(hf_api: HfApi) -> None:
     assert hf_api.repo_exists(DATASET_REPO_NAME, repo_type="dataset")
 
 
-@pytest.mark.skipif(os.environ.get("CI") is not None)
+@pytest.mark.skipif(os.environ.get("CI") is not None, reason="Skipping HF API tests in CI")
 def test_can_access_hf_model_repo(hf_api: HfApi) -> None:
     assert hf_api.repo_exists(MODEL_REPO_NAME, repo_type="model")
